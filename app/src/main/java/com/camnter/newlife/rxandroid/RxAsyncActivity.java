59835b48c9cad2377a62f0f4c0ad9bab9ee82e62
CaMnter <421482590@qq.com>
2015-12-01 15:43:47 +0800

    mod RxSyncActivity

---
+++ app/src/main/java/com/camnter/newlife/rxandroid/RxAsyncActivity.java
@@ -0,0 +1,16 @@
+package com.camnter.newlife.rxandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Description：RxAsyncActivity
+ * Created by：CaMnter
+ * Time：2015-12-01 12:44
+ */
+public class RxAsyncActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
