2edc41380370a474a247cb5c25f4be4e277495d3
CaMnter <421482590@qq.com>
2015-12-06 14:51:09 +0800

    add Md5Util

---
+++ app/src/main/java/com/camnter/newlife/utils/Md5Util.java
@@ -0,0 +1,54 @@
+package com.camnter.newlife.utils;
+
+import java.security.MessageDigest;
+
+/**
+ * Description：Md5Util
+ * Created by：CaMnter
+ * Time：2015-12-06 22:33
+ */
+public class Md5Util {
+
+    public static String getMD5String(String key) {
+        char hexDigits[] = { '0', '1', '2', '3', '4',
+                '5', '6', '7', '8', '9',
+                'A', 'B', 'C', 'D', 'E', 'F' };
+        try {
+            byte[] btInput = key.getBytes();
+            //获得MD5摘要算法的 MessageDigest 对象
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            //使用指定的字节更新摘要
+            mdInst.update(btInput);
+            //获得密文
+            byte[] md = mdInst.digest();
+            //把密文转换成十六进制的字符串形式
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static String bytesToHexString(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte aByte : bytes) {
+            String hex = Integer.toHexString(255 & aByte);
+            if (hex.length() == 1) {
+                sb.append('0');
+            }
+
+            sb.append(hex);
+        }
+        return sb.toString();
+    }
+
+}
