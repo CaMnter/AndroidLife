9b7fd36a0e32ae4a7349e9ea998c48c37c72b99a
CaMnter <421482590@qq.com>
2015-12-16 23:39:49 +0800

    update LayoutParamsUtil

--- app/src/main/java/com/camnter/newlife/utils/LayoutParamsUtil.java
+++ app/src/main/java/com/camnter/newlife/utils/LayoutParamsUtil.java
@@ -61,6 +61,18 @@ public class LayoutParamsUtil {
     }
 
     /**
+     * dp 转化为 px
+     *
+     * @param context context
+     * @param dpValue dpValue
+     * @return int
+     */
+    public static int dp2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
      * Set the marin value of view
      * 设置Marin值
      *
@@ -71,17 +83,74 @@ public class LayoutParamsUtil {
      * @param rightDpValue  rightDpValue
      * @param bottomDpValue bottomDpValue
      */
-    public static void setMargins(Context context, View view, int leftDpValue, int topDpValue, int rightDpValue, int bottomDpValue) {
-        final float scale = context.getResources().getDisplayMetrics().density;
+    public static void setMargins(
+            Context context, View view,
+            int leftDpValue, int topDpValue, int rightDpValue, int bottomDpValue) {
         if (view.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
-            int leftPxValue = (int) (leftDpValue * scale + 0.5f);
-            int topPxValue = (int) (topDpValue * scale + 0.5f);
-            int rightPxValue = (int) (rightDpValue * scale + 0.5f);
-            int bottomPxValue = (int) (bottomDpValue * scale + 0.5f);
             ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
-            layoutParams.setMargins(leftPxValue, topPxValue, rightPxValue, bottomPxValue);
+            layoutParams.setMargins(
+                    dp2px(context, leftDpValue),
+                    dp2px(context, topDpValue),
+                    dp2px(context, rightDpValue),
+                    dp2px(context, bottomDpValue)
+            );
             view.requestLayout();
         }
+    }
+
+
+    /**
+     * Set the width of view
+     * 设置宽度
+     *
+     * @param context      context
+     * @param view         view
+     * @param widthDpValue widthDpValue
+     */
+    public static void setWidth(Context context, View view, int widthDpValue) {
+        if (view.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+            ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
+            layoutParams.width = dp2px(context, widthDpValue);
+            view.requestLayout();
+        }
+    }
+
+    /**
+     * Set the height of view
+     * 设置高度
+     *
+     * @param context       context
+     * @param view          view
+     * @param heightDpValue heightDpValue
+     */
+    public static void setHeight(Context context, View view, int heightDpValue) {
+        if (view.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+            ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
+            layoutParams.height = dp2px(context, heightDpValue);
+            view.requestLayout();
+        }
+    }
+
+    /**
+     * Set the 设置padding值 value of view
+     * 设置padding值
+     *
+     * @param context       context
+     * @param view          view
+     * @param leftDpValue   leftDpValue
+     * @param topDpValue    topDpValue
+     * @param rightDpValue  rightDpValue
+     * @param bottomDpValue bottomDpValue
+     */
+    public static void setPadding(
+            Context context, View view,
+            int leftDpValue, int topDpValue, int rightDpValue, int bottomDpValue) {
+        view.setPadding(
+                dp2px(context, leftDpValue),
+                dp2px(context, topDpValue),
+                dp2px(context, rightDpValue),
+                dp2px(context, bottomDpValue)
+        );
     }
 
 }
