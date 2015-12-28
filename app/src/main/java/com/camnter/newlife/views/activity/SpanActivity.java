1d132fbd26fdc74f8e5900dc98ac988375bab4e1
CaMnter <421482590@qq.com>
2015-12-28 13:14:32 +0800

    update easyrecyclerview1.0 and use EasyRecyclerView,EasyDividerItemDecoration

--- app/src/main/java/com/camnter/newlife/views/activity/SpanActivity.java
+++ app/src/main/java/com/camnter/newlife/views/activity/SpanActivity.java
@@ -2,14 +2,12 @@ package com.camnter.newlife.views.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.camnter.easyrecyclerview.widget.decorator.EasyDividerItemDecoration;
 import com.camnter.newlife.R;
 import com.camnter.newlife.adapter.SpanRecyclerAdapter;
 import com.camnter.newlife.bean.SpanData;
-import com.camnter.newlife.widget.decorator.DividerItemDecoration;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,16 +21,14 @@ public class SpanActivity extends AppCompatActivity {
 
     private static final String CONTENT = "Save you from anything";
 
-    private RecyclerView spanRV;
-    private SpanRecyclerAdapter adapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_span);
-        this.spanRV = (RecyclerView) this.findViewById(R.id.span_rv);
-        this.adapter = new SpanRecyclerAdapter(this);
-        this.initRecyclerView();
+        RecyclerView spanRV = (RecyclerView) this.findViewById(R.id.span_rv);
+        SpanRecyclerAdapter adapter = new SpanRecyclerAdapter(this);
+        spanRV.setAdapter(adapter);
+        spanRV.addItemDecoration(new EasyDividerItemDecoration(this, EasyDividerItemDecoration.VERTICAL_LIST, R.drawable.bg_recycler_view_divider));
 
         List<SpanData> spans = new ArrayList<>();
         SpanData spanData = new SpanData();
@@ -49,22 +45,5 @@ public class SpanActivity extends AppCompatActivity {
         adapter.notifyDataSetChanged();
     }
 
-    private void initRecyclerView() {
-        this.spanRV.setAdapter(this.adapter);
-
-        // 实例化LinearLayoutManager
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        // 设置垂直布局
-        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-
-        // 设置布局管理器
-        this.spanRV.setLayoutManager(linearLayoutManager);
-
-        this.spanRV.setItemAnimator(new DefaultItemAnimator());
-        this.spanRV.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
-
-        // 使RecyclerView保持固定的大小，该信息被用于自身的优化
-        this.spanRV.setHasFixedSize(true);
-    }
 
 }
--- app/src/main/java/com/camnter/newlife/widget/decorator/DividerItemDecoration.java
+++ app/src/main/java/com/camnter/newlife/widget/decorator/DividerItemDecoration.java
@@ -16,11 +16,13 @@
 
 package com.camnter.newlife.widget.decorator;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -42,20 +44,38 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
 
     public DividerItemDecoration(Context context, int orientation) {
         final TypedArray a = context.obtainStyledAttributes(ATTRS);
-        mDivider = a.getDrawable(0);
+        this.mDivider = a.getDrawable(0);
         a.recycle();
-        setOrientation(orientation);
+        this.setOrientation(orientation);
+    }
+
+    public DividerItemDecoration(Activity activity, int orientation, int drawableId) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            this.mDivider = activity.getTheme().getDrawable(drawableId);
+        } else {
+            this.mDivider = activity.getResources().getDrawable(drawableId);
+        }
+        this.setOrientation(orientation);
     }
 
     public void setOrientation(int orientation) {
         if (orientation != HORIZONTAL_LIST && orientation != VERTICAL_LIST) {
             throw new IllegalArgumentException("invalid orientation");
         }
-        mOrientation = orientation;
+        this.mOrientation = orientation;
     }
 
+    /**
+     * Draw any appropriate decorations into the Canvas supplied to the RecyclerView.
+     * Any content drawn by this method will be drawn before the item views are drawn,
+     * and will thus appear underneath the views.
+     *
+     * @param c      Canvas to draw into
+     * @param parent RecyclerView this ItemDecoration is drawing into
+     * @param state  The current state of RecyclerView
+     */
     @Override
-    public void onDraw(Canvas c, RecyclerView parent) {
+    public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
         if (mOrientation == VERTICAL_LIST) {
             drawVertical(c, parent);
         } else {
@@ -68,7 +88,7 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
         final int right = parent.getWidth() - parent.getPaddingRight();
 
         final int childCount = parent.getChildCount();
-        for (int i = 0; i < childCount; i++) {
+        for (int i = 0; i < childCount - 1; i++) {
             final View child = parent.getChildAt(i);
             final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child
                     .getLayoutParams();
@@ -85,7 +105,7 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
         final int bottom = parent.getHeight() - parent.getPaddingBottom();
 
         final int childCount = parent.getChildCount();
-        for (int i = 0; i < childCount; i++) {
+        for (int i = 0; i < childCount - 1; i++) {
             final View child = parent.getChildAt(i);
             final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child
                     .getLayoutParams();
@@ -97,8 +117,28 @@ public class DividerItemDecoration extends RecyclerView.ItemDecoration {
         }
     }
 
+    /**
+     * Retrieve any offsets for the given item. Each field of <code>outRect</code> specifies
+     * the number of pixels that the item view should be inset by, similar to padding or margin.
+     * The default implementation sets the bounds of outRect to 0 and returns.
+     * <p/>
+     * <p/>
+     * If this ItemDecoration does not affect the positioning of item views, it should set
+     * all four fields of <code>outRect</code> (left, top, right, bottom) to zero
+     * before returning.
+     * <p/>
+     * <p/>
+     * If you need to access Adapter for additional data, you can call
+     * {@link RecyclerView#getChildAdapterPosition(View)} to get the adapter position of the
+     * View.
+     *
+     * @param outRect Rect to receive the output.
+     * @param view    The child view to decorate
+     * @param parent  RecyclerView this ItemDecoration is decorating
+     * @param state   The current state of RecyclerView.
+     */
     @Override
-    public void getItemOffsets(Rect outRect, int itemPosition, RecyclerView parent) {
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
         if (mOrientation == VERTICAL_LIST) {
             outRect.set(0, 0, 0, mDivider.getIntrinsicHeight());
         } else {
