5c4c27c8c2c931a536c5ad274aa60f6f1b90fd04
CaMnter <421482590@qq.com>
2016-01-01 17:22:01 +0800

    add some lib

--- app/src/main/java/com/camnter/newlife/views/activity/LocationManagerActivity.java
+++ app/src/main/java/com/camnter/newlife/views/activity/LocationManagerActivity.java
@@ -1,10 +1,13 @@
 package com.camnter.newlife.views.activity;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -65,8 +68,20 @@ public class LocationManagerActivity extends AppCompatActivity {
             }
         };
         this.locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for Activity#requestPermissions for more details.
+                return;
+            }
+        }
         Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        if(location!=null){
+        if (location != null) {
             this.longitudeTV.setText(location.getLongitude() + "");
             this.latitudeTV.setText(location.getLatitude() + "");
             this.altitudeTV.setText(location.getAltitude() + "");
