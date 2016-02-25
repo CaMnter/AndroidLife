d281b8544d5bfd88699c28675d6fd4d1d8692a36
CaMnter <yuanyu.camnter@gmail.com>
2016-02-25 22:26:11 +0800

    add BitmapUtils

---
+++ app/src/main/java/com/camnter/newlife/utils/BitmapUtils.java
@@ -0,0 +1,78 @@
+package com.camnter.newlife.utils;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * Description：BitmapUtils
+ * Created by：CaMnter
+ * Time：2016-02-25 22:23
+ */
+public class BitmapUtils {
+
+    private BitmapUtils() {
+
+    }
+
+    private static final float DENSITY = Resources.getSystem().getDisplayMetrics().density;
+    private static final Canvas sCanvas = new Canvas();
+
+    public static int dp2Px(int dp) {
+        return Math.round(dp * DENSITY);
+    }
+
+    /**
+     * 通过 View 创建 Bitmap
+     *
+     * @param view view
+     * @return Bitmap
+     */
+    public static Bitmap createBitmapFromView(View view) {
+        if (view instanceof ImageView) {
+            Drawable drawable = ((ImageView) view).getDrawable();
+            if (drawable != null && drawable instanceof BitmapDrawable) {
+                return ((BitmapDrawable) drawable).getBitmap();
+            }
+        }
+        view.clearFocus();
+        Bitmap bitmap = createBitmapSafely(view.getWidth(),
+                view.getHeight(), Bitmap.Config.ARGB_8888, 1);
+        if (bitmap != null) {
+            synchronized (sCanvas) {
+                Canvas canvas = sCanvas;
+                canvas.setBitmap(bitmap);
+                view.draw(canvas);
+                canvas.setBitmap(null);
+            }
+        }
+        return bitmap;
+    }
+
+    /**
+     * 安全地创建 Bitmap
+     *
+     * @param width      width
+     * @param height     height
+     * @param config     config
+     * @param retryCount retryCount
+     * @return Bitmap
+     */
+    public static Bitmap createBitmapSafely(int width, int height, Bitmap.Config config, int retryCount) {
+        try {
+            return Bitmap.createBitmap(width, height, config);
+        } catch (OutOfMemoryError e) {
+            e.printStackTrace();
+            if (retryCount > 0) {
+                System.gc();
+                return createBitmapSafely(width, height, config, retryCount - 1);
+            }
+            return null;
+        }
+    }
+
+}
