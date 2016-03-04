5f0f46848d59796f63c43878b3ef534735e6fcaa
CaMnter <yuanyu.camnter@gmail.com>
2016-03-04 12:02:18 +0800

    Fix don't need permission

--- app/src/main/java/com/camnter/newlife/views/activity/util/DeviceUtilActivity.java
+++ app/src/main/java/com/camnter/newlife/views/activity/util/DeviceUtilActivity.java
@@ -1,8 +1,10 @@
 package com.camnter.newlife.views.activity.util;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.widget.TextView;
@@ -85,62 +87,57 @@ public class DeviceUtilActivity extends BaseAppCompatActivity {
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.READ_PHONE_STATE)
                 != PackageManager.PERMISSION_GRANTED) {
-
             // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.READ_PHONE_STATE)) {
-
                 // Show an expanation to the user *asynchronously* -- don't block
                 // this thread waiting for the user's response! After the user
                 // sees the explanation, try again to request the permission.
-
             } else {
-
                 // No explanation needed, we can request the permission.
-
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.READ_PHONE_STATE},
                         READ_PHONE_STATE_REQUEST);
-
                 // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
                 // app-defined int constant. The callback method gets the
                 // result of the request.
             }
+        } else {
+            this.setData();
         }
+    }
 
+    @SuppressLint("SetTextI18n")
+    private void setData() {
+        this.deviceIdTV.setText(DeviceUtils.getDeviceId(this));
+        this.versionCodeTV.setText(DeviceUtils.getVersionCode(this));
+        this.versionNameTV.setText(DeviceUtils.getVersionName(this));
+        this.phoneBrandTV.setText(DeviceUtils.getPhoneBrand());
+        this.phoneModelTV.setText(DeviceUtils.getPhoneModel());
+        this.apiLevelTV.setText(DeviceUtils.getBuildLevel() + "");
+        this.apiVersionTV.setText(DeviceUtils.getBuildVersion());
+        this.appProcessIdTV.setText(DeviceUtils.getAppProcessId() + "");
+        this.appNameTV.setText(DeviceUtils.getAppProcessName(this, DeviceUtils.getAppProcessId()));
+        this.metaDataTV.setText(DeviceUtils.getMetaData(this, "DEBUG"));
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
         switch (requestCode) {
             case READ_PHONE_STATE_REQUEST: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
                     // permission was granted, yay! Do the
                     // contacts-related task you need to do.
-                    this.deviceIdTV.setText(DeviceUtils.getDeviceId(this));
-                    this.versionCodeTV.setText(DeviceUtils.getVersionCode(this));
-                    this.versionNameTV.setText(DeviceUtils.getVersionName(this));
-
-                    this.phoneBrandTV.setText(DeviceUtils.getPhoneBrand());
-                    this.phoneModelTV.setText(DeviceUtils.getPhoneModel());
-                    this.apiLevelTV.setText(DeviceUtils.getBuildLevel() + "");
-                    this.apiVersionTV.setText(DeviceUtils.getBuildVersion());
-                    this.appProcessIdTV.setText(DeviceUtils.getAppProcessId() + "");
-                    this.appNameTV.setText(DeviceUtils.getAppProcessName(this, DeviceUtils.getAppProcessId()));
-                    this.metaDataTV.setText(DeviceUtils.getMetaData(this, "DEBUG"));
-
+                    this.setData();
                 } else {
-
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
+                    this.showToast("没有权限访问");
                 }
-                return;
             }
-
             // other 'case' lines to check for other
             // permissions this app might request
         }
