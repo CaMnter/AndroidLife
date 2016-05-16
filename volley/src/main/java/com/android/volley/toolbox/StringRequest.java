f2db077f894f58d40cade464b0d9cd4d5d3d9cda
CaMnter <yuanyu.camnter@gmail.com>
2016-05-17 00:39:12 +0800

    add StringRequest comments in volley module

--- volley/src/main/java/com/android/volley/toolbox/StringRequest.java
+++ volley/src/main/java/com/android/volley/toolbox/StringRequest.java
@@ -27,6 +27,12 @@ import java.io.UnsupportedEncodingException;
 /**
  * A canned request for retrieving the response body at a given URL as a String.
  */
+
+/*
+ * 继承扩展了 Request，指定了泛型为 <String>
+ * 会将请求结果解析成 String 类型数据
+ * 并且 需要你 传入一个 Response.Listener<String> 进行解析结果数据进行回调
+ */
 public class StringRequest extends Request<String> {
     private final Listener<String> mListener;
 
@@ -51,23 +57,43 @@ public class StringRequest extends Request<String> {
      * @param listener Listener to receive the String response
      * @param errorListener Error listener, or null to ignore errors
      */
+    /*
+     * 默认的 请求方法 为 GET 请求方法
+     */
     public StringRequest(String url, Listener<String> listener, ErrorListener errorListener) {
         this(Method.GET, url, listener, errorListener);
     }
 
+    /*
+     * 扩展实现传递 数据方法
+     * 由于指定了泛型 <String>，所以这里也只能 传递 String 类型数据
+     */
     @Override
     protected void deliverResponse(String response) {
         mListener.onResponse(response);
     }
 
+    /*
+     * 解析网络请求 为 String 类型
+     */
     @Override
     protected Response<String> parseNetworkResponse(NetworkResponse response) {
         String parsed;
         try {
+            /*
+             * 1. HttpHeaderParser 解析编码集
+             * 2. 将请求结果 Response 的数据 （ data ）通过编码集实例化一个数据 （ data ） String 类型
+             */
             parsed = new String(response.data, HttpHeaderParser.parseCharset(response.headers));
         } catch (UnsupportedEncodingException e) {
+            // 出现编码异常，就不进行编码处理，直接实例化一个 String
             parsed = new String(response.data);
         }
+        /*
+         * 解析成功，没有异常
+         * 1. 将保存好 通过编码解析的 String 数据 返回
+         * 2. 再通过 HttpHeaderParser 从网络请求回来的请求结果 NetworkResponse 的 Header 中提取出一个用于缓存的 Cache.Entry
+         */
         return Response.success(parsed, HttpHeaderParser.parseCacheHeaders(response));
     }
 }
