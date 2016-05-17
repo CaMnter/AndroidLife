cb9c9f359caed2745c6e3b2220a9e3d1f846eb7b
CaMnter <yuanyu.camnter@gmail.com>
2016-05-17 13:53:14 +0800

    add Authenticator comments in volley module

--- volley/src/main/java/com/android/volley/toolbox/Authenticator.java
+++ volley/src/main/java/com/android/volley/toolbox/Authenticator.java
@@ -21,16 +21,27 @@ import com.android.volley.AuthFailureError;
 /**
  * An interface for interacting with auth tokens.
  */
+/*
+ * Authenticator 是一个身份认证接口
+ * 用于基本认证或者摘要认证
+ * Volley内，是用于和身份认证，比如 OAuth
+ */
 public interface Authenticator {
     /**
      * Synchronously retrieves an auth token.
      *
      * @throws AuthFailureError If authentication did not succeed
      */
+    /*
+     * 获取授权 token
+     */
     public String getAuthToken() throws AuthFailureError;
 
     /**
      * Invalidates the provided auth token.
+     */
+    /*
+     * 无效授权 token
      */
     public void invalidateAuthToken(String authToken);
 }
