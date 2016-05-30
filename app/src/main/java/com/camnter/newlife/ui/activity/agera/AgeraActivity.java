2a4e1fad6bc6dd9ee82e43e1a2f5e726021cade5
CaMnter <yuanyu.camnter@gmail.com>
2016-05-30 16:20:25 +0800

    set lintOptions in agera module

---
+++ app/src/main/java/com/camnter/newlife/ui/activity/agera/AgeraActivity.java
@@ -0,0 +1,14 @@
+package com.camnter.newlife.ui.activity.agera;
+
+import com.camnter.newlife.ui.activity.MainActivity;
+
+/**
+ * Description：AgeraActivity
+ * Created by：CaMnter
+ * Time：2016-05-30 15:24
+ */
+public class AgeraActivity extends MainActivity {
+    @Override protected void setListData() {
+        this.showTag = false;
+    }
+}
