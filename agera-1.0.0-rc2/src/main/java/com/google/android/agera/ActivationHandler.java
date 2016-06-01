4ae40cf7cc47e0a48bfd2a8c651bd17f91f835d5
CaMnter <yuanyu.camnter@gmail.com>
2016-06-02 00:27:51 +0800

    add ActivationHandler comments in agera-1.0.0-rc2 module

--- agera-1.0.0-rc2/src/main/java/com/google/android/agera/ActivationHandler.java
+++ agera-1.0.0-rc2/src/main/java/com/google/android/agera/ActivationHandler.java
@@ -19,7 +19,8 @@ import android.support.annotation.NonNull;
 
 /**
  * Receives events of the {@link UpdateDispatcher} created with
- * {@link Observables#updateDispatcher(ActivationHandler)} when the first {@link Updatable} is added
+ * {@link Observables#updateDispatcher(ActivationHandler)} when the first {@link Updatable} is
+ * added
  * and the last {@link Updatable} is removed.
  *
  * <p>Typically an {@link Observable} service implemented using a {@link UpdateDispatcher} only
@@ -27,18 +28,27 @@ import android.support.annotation.NonNull;
  * clients on {@link #observableActivated} and stopping on {@link #observableDeactivated}, the
  * service of the service can implement an <i>active</i>/<i>inactive</i> lifecycle,
  * saving memory and execution time when not needed.
+ *
+ * Agera 中抽象出来的 激活处理 接口
+ * 用于 执行 被观察者 的 激活 和 释放 功能
+ * 这里的 被观察者 是 UpdateDispatcher
+ * 因为 UpdateDispatcher 继承了 被观察者（ Observable ）接口 和 观察者（ Updatable ）接口
  */
 public interface ActivationHandler {
 
-  /**
-   * Called when the the {@code caller} changes state from having no {@link Updatable}s to
-   * having at least one {@link Updatable}.
-   */
-  void observableActivated(@NonNull UpdateDispatcher caller);
+    /**
+     * Called when the the {@code caller} changes state from having no {@link Updatable}s to
+     * having at least one {@link Updatable}.
+     *
+     * 被观察者 被 激活时
+     */
+    void observableActivated(@NonNull UpdateDispatcher caller);
 
-  /**
-   * Called when the the {@code caller} changes state from having {@link Updatable}s to
-   * no longer having {@link Updatable}s.
-   */
-  void observableDeactivated(@NonNull UpdateDispatcher caller);
+    /**
+     * Called when the the {@code caller} changes state from having {@link Updatable}s to
+     * no longer having {@link Updatable}s.
+     *
+     * 被观察者 被 释放时
+     */
+    void observableDeactivated(@NonNull UpdateDispatcher caller);
 }
