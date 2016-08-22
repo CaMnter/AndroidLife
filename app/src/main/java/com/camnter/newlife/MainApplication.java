ce6734785c960001017d637d1bd46ef874ed1751
CaMnter <yuanyu.camnter@gmail.com>
2016-08-22 23:25:34 +0800

    [ Add ] hotfix module

--- app/src/main/java/com/camnter/newlife/MainApplication.java
+++ app/src/main/java/com/camnter/newlife/MainApplication.java
@@ -1,6 +1,10 @@
 package com.camnter.newlife;
 
 import android.app.Application;
+import android.content.Context;
+import com.camnter.hotfix.HotFix;
+import com.camnter.newlife.utils.AssetsUtils;
+import java.io.File;
 
 /**
  * Description：MainApplication
@@ -20,5 +24,35 @@ public class MainApplication extends Application {
     @Override public void onCreate() {
         instance = this;
         super.onCreate();
+        this.insertPile();
+        this.insertPatch();
     }
+
+
+    /**
+     * 插桩
+     */
+    private void insertPile() {
+        File dexPath = new File(getDir("dex", Context.MODE_PRIVATE), "hack.jar");
+        AssetsUtils.prepareDex(this.getApplicationContext(), dexPath, "hack.jar");
+        HotFix.patch(this, dexPath.getAbsolutePath(), "com.camnter.hack.AntilazyLoad");
+        try {
+            this.getClassLoader().loadClass("com.camnter.hack.AntilazyLoad");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 打补丁
+     */
+    private void insertPatch() {
+        //准备补丁,从 assert 里拷贝到 dex 里
+        File dexPath = new File(getDir("dex", Context.MODE_PRIVATE), "patch_dex.jar");
+        AssetsUtils.prepareDex(this.getApplicationContext(), dexPath, "patch_dex.jar");
+        HotFix.patch(this, dexPath.getAbsolutePath(),
+            "com.camnter.newlife.ui.activity.hotfix.FixCall");
+    }
+
 }
