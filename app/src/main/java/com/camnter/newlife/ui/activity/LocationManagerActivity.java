f77611427cb76ff3afe5c03fe4996743a0bfbc02
CaMnter <yuanyu.camnter@gmail.com>
2016-08-29 14:55:17 +0800

    [Remove] useless TODO

--- app/src/main/java/com/camnter/newlife/ui/activity/LocationManagerActivity.java
+++ app/src/main/java/com/camnter/newlife/ui/activity/LocationManagerActivity.java
@@ -128,7 +128,6 @@ public class LocationManagerActivity extends BaseAppCompatActivity {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
             PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
             Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
             //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
