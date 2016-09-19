64fa0be4ac5781e61193f8b16bcecbfd8a449507
CaMnter <yuanyu.camnter@gmail.com>
2016-09-19 22:18:47 +0800

    [Add] test in app module

---
+++ app/src/test/java/com/camnter/newlife/utils/AssetsUtilsTest.java
@@ -0,0 +1,21 @@
+package com.camnter.newlife.utils;
+
+import org.junit.Test;
+
+/**
+ * Description：
+ * Created by：CaMnter
+ */
+public class AssetsUtilsTest {
+    @Test
+    public void prepareDex() throws Exception {
+
+    }
+
+
+    @Test
+    public void copyAssets() throws Exception {
+
+    }
+
+}
