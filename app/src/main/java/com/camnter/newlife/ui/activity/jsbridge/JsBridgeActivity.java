f3754f37506d2d4051b174e0235f7c05f2e46ce8
CaMnter <yuanyu.camnter@gmail.com>
2016-09-23 17:42:43 +0800

    [Fix] index.html and JsBridgeActivity

--- app/src/main/java/com/camnter/newlife/ui/activity/jsbridge/JsBridgeActivity.java
+++ app/src/main/java/com/camnter/newlife/ui/activity/jsbridge/JsBridgeActivity.java
@@ -1,10 +1,16 @@
 package com.camnter.newlife.ui.activity.jsbridge;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.net.http.SslError;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -16,7 +22,7 @@ import com.camnter.newlife.core.BaseAppCompatActivity;
  * Created by：CaMnter
  */
 
-public class JsBridgeActivity extends BaseAppCompatActivity implements View.OnClickListener {
+public class JsBridgeActivity extends BaseAppCompatActivity {
 
     @BindView(R.id.js_bridge_webview) WebView jsBridgeWebview;
     @BindView(R.id.js_bridge_button) Button jsBridgeButton;
@@ -43,9 +49,43 @@ public class JsBridgeActivity extends BaseAppCompatActivity implements View.OnCl
         this.setContentView(R.layout.activity_js_bridge);
 
         WebSettings settings = this.jsBridgeWebview.getSettings();
-        settings.setJavaScriptEnabled(true);
-        this.jsBridgeWebview.setWebChromeClient(new JsBridgeWebChromeClient());
-        this.jsBridgeWebview.loadUrl("file:///android_asset/index.html");
+        // settings.setJavaScriptEnabled(true);
+        // this.jsBridgeWebview.setWebChromeClient(new JsBridgeWebChromeClient());
+        // this.jsBridgeWebview.loadUrl("file:///android_asset/index.html");
+
+        this.jsBridgeWebview.getSettings().setSupportZoom(false);
+        //      this.webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        this.jsBridgeWebview.getSettings().setJavaScriptEnabled(true);
+        jsBridgeWebview.setWebViewClient(new WebViewClient() {
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                //handler.cancel(); 默认的处理方式，WebView变成空白页
+                //                        //接受证书
+                handler.proceed();
+                //handleMessage(Message msg); 其他处理
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                if (jsBridgeWebview.getVisibility() != View.VISIBLE) {
+                    jsBridgeWebview.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+
+
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP) @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+        });
+
+        this.jsBridgeWebview.loadUrl("https://www.baidu.com/");
 
         JsBridge.register("bridge", BridgeImpl.class);
     }
@@ -55,7 +95,6 @@ public class JsBridgeActivity extends BaseAppCompatActivity implements View.OnCl
      * Initialize the View of the listener
      */
     @Override protected void initListeners() {
-        this.jsBridgeButton.setOnClickListener(this);
     }
 
 
@@ -72,11 +111,7 @@ public class JsBridgeActivity extends BaseAppCompatActivity implements View.OnCl
      *
      * @param v The view that was clicked.
      */
-    @Override public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.js_bridge_button:
-                this.jsBridgeWebview.loadUrl("file:///android_asset/index.html");
-                break;
-        }
+    public void onButtonClick(View v) {
+        this.jsBridgeWebview.loadUrl("https://www.baidu.com/");
     }
 }
