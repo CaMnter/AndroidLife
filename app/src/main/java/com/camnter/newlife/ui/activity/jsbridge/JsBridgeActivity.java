b0b85714cde5e1b9ea5cd16b6f6af4194f9df27b
CaMnter <yuanyu.camnter@gmail.com>
2016-09-23 18:15:07 +0800

    [Fix] remote the setContentView(...)

--- app/src/main/java/com/camnter/newlife/ui/activity/jsbridge/JsBridgeActivity.java
+++ app/src/main/java/com/camnter/newlife/ui/activity/jsbridge/JsBridgeActivity.java
@@ -1,17 +1,8 @@
 package com.camnter.newlife.ui.activity.jsbridge;
 
-import android.annotation.SuppressLint;
-import android.annotation.TargetApi;
-import android.net.http.SslError;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.webkit.SslErrorHandler;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Button;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.camnter.newlife.R;
@@ -25,7 +16,6 @@ import com.camnter.newlife.core.BaseAppCompatActivity;
 public class JsBridgeActivity extends BaseAppCompatActivity {
 
     @BindView(R.id.js_bridge_webview) WebView jsBridgeWebview;
-    @BindView(R.id.js_bridge_button) Button jsBridgeButton;
 
 
     /**
@@ -43,49 +33,14 @@ public class JsBridgeActivity extends BaseAppCompatActivity {
      *
      * @param savedInstanceState savedInstanceState
      */
-    @SuppressLint("SetJavaScriptEnabled") @Override
+    @Override
     protected void initViews(Bundle savedInstanceState) {
         ButterKnife.bind(this);
-        this.setContentView(R.layout.activity_js_bridge);
 
         WebSettings settings = this.jsBridgeWebview.getSettings();
-        // settings.setJavaScriptEnabled(true);
-        // this.jsBridgeWebview.setWebChromeClient(new JsBridgeWebChromeClient());
-        // this.jsBridgeWebview.loadUrl("file:///android_asset/index.html");
-
-        this.jsBridgeWebview.getSettings().setSupportZoom(false);
-        //      this.webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        this.jsBridgeWebview.getSettings().setJavaScriptEnabled(true);
-        jsBridgeWebview.setWebViewClient(new WebViewClient() {
-            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-                //handler.cancel(); 默认的处理方式，WebView变成空白页
-                //                        //接受证书
-                handler.proceed();
-                //handleMessage(Message msg); 其他处理
-            }
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                if (jsBridgeWebview.getVisibility() != View.VISIBLE) {
-                    jsBridgeWebview.setVisibility(View.VISIBLE);
-                }
-            }
-
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
-
-
-
-            @TargetApi(Build.VERSION_CODES.LOLLIPOP) @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                view.loadUrl(request.getUrl().toString());
-                return true;
-            }
-        });
-
-        this.jsBridgeWebview.loadUrl("https://www.baidu.com/");
+        settings.setJavaScriptEnabled(true);
+        this.jsBridgeWebview.setWebChromeClient(new JsBridgeWebChromeClient());
+        this.jsBridgeWebview.loadUrl("file:///android_asset/index.html");
 
         JsBridge.register("bridge", BridgeImpl.class);
     }
@@ -105,13 +60,4 @@ public class JsBridgeActivity extends BaseAppCompatActivity {
 
     }
 
-
-    /**
-     * Called when a view has been clicked.
-     *
-     * @param v The view that was clicked.
-     */
-    public void onButtonClick(View v) {
-        this.jsBridgeWebview.loadUrl("https://www.baidu.com/");
-    }
 }
