65ed92c31f153f1d446c09ca494e8ef53340c7c8
CaMnter <yuanyu.camnter@gmail.com>
2016-09-27 23:44:50 +0800

    [Add] UtilsAnnotationTest

---
+++ app/src/test/java/com/camnter/newlife/UtilsAnnotationTest.java
@@ -0,0 +1,19 @@
+package com.camnter.newlife;
+
+import com.camnter.newlife.utils.Base64UtilsTest;
+import com.camnter.newlife.utils.ChineseUtilsTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Description：UtilsAnnotationTest
+ * Created by：CaMnter
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+                        Base64UtilsTest.class,
+                        ChineseUtilsTest.class,
+                    })
+public class UtilsAnnotationTest {
+}
