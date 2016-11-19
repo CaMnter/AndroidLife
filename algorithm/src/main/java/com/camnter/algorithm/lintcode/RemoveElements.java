fd8efd8d647bee9540f01988764ddc7ba2c19bad
CaMnter <yuanyu.camnter@gmail.com>
2016-11-19 17:41:21 +0800

    [Add] RemoveElements

---
+++ algorithm/src/main/java/com/camnter/algorithm/lintcode/RemoveElements.java
@@ -0,0 +1,42 @@
+package com.camnter.algorithm.lintcode;
+
+/**
+ * Description：删除链表中的元素
+ *
+ * 删除链表中等于给定值val的所有节点。
+ *
+ * 样例
+ * 给出链表 1->2->3->3->4->5->3, 和 val = 3, 你需要返回删除3之后的链表：1->2->4->5。
+ *
+ * Created by：CaMnter
+ */
+
+public class RemoveElements {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+
+        ListNode(int x) { val = x; }
+    }
+
+
+    public ListNode removeElements(ListNode head, int val) {
+        // Write your code here
+        if (head == null) return null;
+        ListNode first = head, second = head.next;
+        while (second != null) {
+            if (second.val == val) {
+                first.next = second.next;
+                second = second.next;
+            } else {
+                first = first.next;
+                second = second.next;
+            }
+
+        }
+        return head.val != val ? head : head.next;
+    }
+
+}
