91ef9f9141448bbbb8ad36d8d715d169f763a85d
CaMnter <yuanyu.camnter@gmail.com>
2017-02-17 23:51:07 +0800

    [Add] ViewPagerTab in app module

---
+++ app/src/main/java/com/camnter/newlife/widget/tab/ViewPagerTab.java
@@ -0,0 +1,133 @@
+package com.camnter.newlife.widget.tab;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.Scroller;
+
+/**
+ * Description：ViewPagerTab
+ * Created by：CaMnter
+ */
+
+public class ViewPagerTab extends ViewGroup {
+
+    private PageListener mPageListener = new PageListener();
+
+    private int mWidth;
+    private int mHeight;
+    private int itemCount;
+    private Scroller mScroller;
+    // 120 px
+    private int tabLineSize = 120;
+
+
+    public ViewPagerTab(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        Context mContext = context;
+        mScroller = new Scroller(mContext);
+    }
+
+
+    public void setItemCount(int count) {
+        this.itemCount = count;
+    }
+
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        if (this.getChildCount() <= 0) return;
+        this.getChildAt(0).layout(
+            (mWidth / itemCount - tabLineSize) / 2,
+            0,
+            (mWidth / itemCount - tabLineSize) / 2 + tabLineSize,
+            mHeight
+        );
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        mWidth = MeasureSpec.getSize(widthMeasureSpec);
+        mHeight = MeasureSpec.getSize(heightMeasureSpec);
+    }
+
+
+    public void setViewPager(ViewPager viewPager) {
+        ViewPager mViewPager = viewPager;
+        mViewPager.addOnPageChangeListener(mPageListener);
+    }
+
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if (mScroller.computeScrollOffset()) {
+            scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
+            postInvalidate();
+        }
+    }
+
+
+    public interface OnPageSelected {
+        public abstract void onPageSelected(int position);
+    }
+
+
+    private OnPageSelected listener = null;
+
+
+    public void setOnPageSelectedListener(OnPageSelected listener) {
+        this.listener = listener;
+    }
+
+
+    private class PageListener implements ViewPager.OnPageChangeListener {
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            scrollTo(
+                -position * mWidth / itemCount - Math.round(positionOffset * mWidth / itemCount),
+                0);
+        }
+
+
+        @Override
+        public void onPageSelected(int position) {
+
+            if (listener != null) {
+                listener.onPageSelected(position);
+            }
+        }
+
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+
+    }
+
+
+    public PageListener getPagerListener() {
+        return mPageListener;
+    }
+
+
+    public void setPagerListener(PageListener listener) {
+        this.mPageListener = listener;
+    }
+
+
+    public void setViewTabWidth(int tab_line_width) {
+        this.tabLineSize = tab_line_width;
+    }
+
+
+    public void setTabLineSize(int tabLineSize) {
+        this.tabLineSize = tabLineSize;
+    }
+
+}
