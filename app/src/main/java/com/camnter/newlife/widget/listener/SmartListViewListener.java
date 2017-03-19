f03cce37d917d3fd4c3d4ae9ae46974e9ae1c987
CaMnter <yuanyu.camnter@gmail.com>
2017-03-19 22:16:22 +0800

    [Add] SmartListViewListener in app module

---
+++ app/src/main/java/com/camnter/newlife/widget/listener/SmartListViewListener.java
@@ -0,0 +1,59 @@
+package com.camnter.newlife.widget.listener;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AbsListView;
+
+/**
+ * Description：SmartListViewListener
+ * Created by：CaMnter
+ */
+
+public abstract class SmartListViewListener implements AbsListView.OnScrollListener {
+
+    private static final String TAG = SmartListViewListener.class.getSimpleName();
+
+
+    protected abstract void onScrollToBottom();
+
+
+    protected void onTouchScroll() {
+    }
+
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+        Log.d(TAG, "[ListViewScrollToBottomListener]\t\t[onScrollStateChanged]\n[scrollState] = " +
+            scrollState);
+        // 当不滚动时
+        if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_IDLE) {
+            // 判断是否滚动到底部
+            if (view.getLastVisiblePosition() == view.getCount() - 1) {
+                this.onScrollToBottom();
+            }
+        } else if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+            this.onTouchScroll();
+        }
+    }
+
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        // Nothing to do
+    }
+
+
+    protected void hiddenKeyBoard(@NonNull final Activity activity) {
+        View focusView = activity.getCurrentFocus();
+        if (focusView == null) return;
+        ((InputMethodManager) activity
+            .getSystemService(Context.INPUT_METHOD_SERVICE))
+            .hideSoftInputFromWindow(focusView.getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+
+}
