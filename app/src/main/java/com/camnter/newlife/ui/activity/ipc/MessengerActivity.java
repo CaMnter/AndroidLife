1e4941e447f09d6272e0b9a34bd1aa2157a99e26
CaMnter <yuanyu.camnter@gmail.com>
2017-05-14 21:47:55 +0800

    [Update] MessengerService in app module

--- app/src/main/java/com/camnter/newlife/ui/activity/ipc/MessengerActivity.java
+++ app/src/main/java/com/camnter/newlife/ui/activity/ipc/MessengerActivity.java
@@ -11,13 +11,26 @@ import android.os.Looper;
 import android.os.Message;
 import android.os.Messenger;
 import android.os.RemoteException;
+import android.support.annotation.NonNull;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.View;
+import android.widget.Toast;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import com.camnter.newlife.R;
 import com.camnter.newlife.core.activity.BaseAppCompatActivity;
+import com.camnter.newlife.utils.ToastUtils;
 import com.camnter.newlife.utils.ipc.MessengerService;
+import java.util.ArrayList;
+
+import static com.camnter.newlife.utils.ipc.MessengerService.BUNDLE_SPARSE_PARCELABLE_BUNDLE;
+import static com.camnter.newlife.utils.ipc.MessengerService.BUNDLE_SPARSE_PARCELABLE_BUNDLE_ARRAY_LIST;
+import static com.camnter.newlife.utils.ipc.MessengerService.BUNDLE_SPARSE_PARCELABLE_BUNDLE_ID_INDEX;
+import static com.camnter.newlife.utils.ipc.MessengerService.BUNDLE_SPARSE_PARCELABLE_BUNDLE_PATH_INDEX;
+import static com.camnter.newlife.utils.ipc.MessengerService.CALLBACK_BUNDLE_ARRAY;
+import static com.camnter.newlife.utils.ipc.MessengerService.CALLBACK_BUNDLE_ARRAY_LIST;
+import static com.camnter.newlife.utils.ipc.MessengerService.CALLBACK_SPARSE_PARCELABLE_BUNDLE;
 
 /**
  * @author CaMnter
@@ -76,24 +89,70 @@ public class MessengerActivity extends BaseAppCompatActivity {
     }
 
 
-    @OnClick(R.id.messenger_send_text) public void onViewClicked(View v) {
+    @OnClick({ R.id.messenger_send_text, R.id.messenger_send_array_text,
+                 R.id.messenger_send_sparse_array })
+    public void onViewClicked(View v) {
         switch (v.getId()) {
             case R.id.messenger_send_text:
-                this.requestMessengerService();
+                this.requestMessengerService("https://www.camnter.com");
+                break;
+            case R.id.messenger_send_array_text:
+                this.requestMessengerService(new String[] { "Save", "you", "from", "anything" },
+                    new ArrayList<String>() {
+                        {
+                            this.add("Save");
+                            this.add("you");
+                            this.add("from");
+                            this.add("anything");
+                        }
+                    });
+                break;
+            case R.id.messenger_send_sparse_array:
+                this.requestMessengerService(
+                    new ArrayList<String>() {
+                        {
+                            this.add("https://www.camnter.com/1");
+                            this.add("https://www.camnter.com/2");
+                            this.add("https://www.camnter.com/3");
+                            this.add("https://www.camnter.com/4");
+                        }
+                    },
+                    new ArrayList<String>() {
+                        {
+                            this.add("id: 2233");
+                            this.add("id: 2333");
+                        }
+                    }
+                );
                 break;
         }
     }
 
 
-    private void requestMessengerService() {
+    private void requestMessengerService(@NonNull final String path) {
         if (this.connection == null) {
-            this.bindMessengerService();
+            this.initCallbackMessenger();
+            this.connection = new ServiceConnection() {
+                @Override public void onServiceConnected(ComponentName name, IBinder service) {
+                    sendMessenger = new Messenger(service);
+                    try {
+                        sendMessenger.send(createRequestMessage("https://www.camnter.com"));
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+                @Override public void onServiceDisconnected(ComponentName name) {
+
+                }
+            };
             Intent intent = new Intent(this, MessengerService.class);
             intent.setAction("com.camnter.ipc.messenger");
             this.bindService(intent, this.connection, Context.BIND_AUTO_CREATE);
         } else {
             try {
-                this.sendMessenger.send(this.createRequestMessage());
+                this.sendMessenger.send(this.createRequestMessage(path));
             } catch (RemoteException e) {
                 e.printStackTrace();
             }
@@ -101,12 +160,76 @@ public class MessengerActivity extends BaseAppCompatActivity {
     }
 
 
-    private Message createRequestMessage() {
+    private void requestMessengerService(@NonNull final String[] stringArray,
+                                         @NonNull final ArrayList<String> arrayList) {
+        if (this.connection == null) {
+            this.initCallbackMessenger();
+            this.connection = new ServiceConnection() {
+                @Override public void onServiceConnected(ComponentName name, IBinder service) {
+                    sendMessenger = new Messenger(service);
+                    try {
+                        sendMessenger.send(createRequestMessage(stringArray, arrayList));
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+                @Override public void onServiceDisconnected(ComponentName name) {
+
+                }
+            };
+            Intent intent = new Intent(this, MessengerService.class);
+            intent.setAction("com.camnter.ipc.messenger");
+            this.bindService(intent, this.connection, Context.BIND_AUTO_CREATE);
+        } else {
+            try {
+                this.sendMessenger.send(this.createRequestMessage(stringArray, arrayList));
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private void requestMessengerService(@NonNull final ArrayList<String> pathList,
+                                         @NonNull final ArrayList<String> idList) {
+        if (this.connection == null) {
+            this.initCallbackMessenger();
+            this.connection = new ServiceConnection() {
+                @Override public void onServiceConnected(ComponentName name, IBinder service) {
+                    sendMessenger = new Messenger(service);
+                    try {
+                        sendMessenger.send(createRequestMessage(pathList, idList));
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+                @Override public void onServiceDisconnected(ComponentName name) {
+
+                }
+            };
+            Intent intent = new Intent(this, MessengerService.class);
+            intent.setAction("com.camnter.ipc.messenger");
+            this.bindService(intent, this.connection, Context.BIND_AUTO_CREATE);
+        } else {
+            try {
+                this.sendMessenger.send(this.createRequestMessage(pathList, idList));
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private Message createRequestMessage(@NonNull final String path) {
         Message message = Message.obtain(null,
             MessengerService.ENCODING_REQUEST_TASK);
         Bundle bundle = new Bundle();
         bundle.putString(MessengerService.BUNDLE_KEY_PATH,
-            "https://www.camnter.com");
+            path);
         message.setData(bundle);
 
         message.replyTo = this.serviceCallBackMessenger;
@@ -114,35 +237,90 @@ public class MessengerActivity extends BaseAppCompatActivity {
     }
 
 
-    private void bindMessengerService() {
-        Handler mainHandler = new Handler(Looper.getMainLooper()) {
+    private Message createRequestMessage(@NonNull final ArrayList<String> pathList,
+                                         @NonNull final ArrayList<String> idList) {
+        Message message = Message.obtain(null,
+            MessengerService.SPARSE_PARCELABLE_BUNDLE_TASK);
+        Bundle bundle = new Bundle();
+
+        Bundle pathBundle = new Bundle();
+        pathBundle.putStringArrayList(BUNDLE_SPARSE_PARCELABLE_BUNDLE_ARRAY_LIST, pathList);
+        Bundle idBundle = new Bundle();
+        idBundle.putStringArrayList(BUNDLE_SPARSE_PARCELABLE_BUNDLE_ARRAY_LIST, idList);
+        SparseArray<Bundle> sparseArray = new SparseArray<>();
+        sparseArray.put(BUNDLE_SPARSE_PARCELABLE_BUNDLE_PATH_INDEX, pathBundle);
+        sparseArray.put(BUNDLE_SPARSE_PARCELABLE_BUNDLE_ID_INDEX, idBundle);
+
+        bundle.putSparseParcelableArray(BUNDLE_SPARSE_PARCELABLE_BUNDLE, sparseArray);
+
+        message.setData(bundle);
+
+        message.replyTo = this.serviceCallBackMessenger;
+        return message;
+    }
+
+
+    private Message createRequestMessage(@NonNull final String[] stringArray,
+                                         @NonNull final ArrayList<String> arrayList) {
+        Message message = Message.obtain(null,
+            MessengerService.ARRAY_REQUEST_TASK);
+        Bundle bundle = new Bundle();
+        bundle.putStringArray(MessengerService.BUNDLE_ARRAY, stringArray);
+        bundle.putStringArrayList(MessengerService.BUNDLE_ARRAY_LIST, arrayList);
+        message.setData(bundle);
+
+        message.replyTo = this.serviceCallBackMessenger;
+        return message;
+    }
+
+
+    private void initCallbackMessenger() {
+        this.serviceCallBackMessenger = new Messenger(new Handler(Looper.getMainLooper()) {
             @Override public void handleMessage(Message msg) {
+                Bundle bundle;
                 switch (msg.what) {
                     case MessengerService.ENCODING_REQUEST_TASK_CALL_BACK:
-                        Bundle bundle = msg.getData();
-                        Log.e(TAG, "[BUNDLE_REQUEST_CODE] = " +
-                            bundle.getInt(MessengerService.BUNDLE_REQUEST_CODE));
+                        bundle = msg.getData();
+                        ToastUtils.show(MessengerActivity.this, "[BUNDLE_REQUEST_CODE] = " +
+                                bundle.getInt(MessengerService.BUNDLE_REQUEST_CODE),
+                            Toast.LENGTH_LONG);
+                        break;
+                    case MessengerService.ARRAY_REQUEST_TASK_CALL_BACK:
+                        bundle = msg.getData();
+                        String[] stringArray = bundle.getStringArray(CALLBACK_BUNDLE_ARRAY);
+                        ArrayList<String> stringList = bundle.getStringArrayList(
+                            CALLBACK_BUNDLE_ARRAY_LIST);
+                        String stringText = stringArray != null
+                                            ? String.valueOf(stringArray.length)
+                                            : "null";
+                        ToastUtils.show(MessengerActivity.this, "[CALLBACK_BUNDLE_ARRAY] = " +
+                                stringText + "  [CALLBACK_BUNDLE_ARRAY_LIST] = " +
+                                stringList,
+                            Toast.LENGTH_LONG);
+                        break;
+                    case MessengerService.SPARSE_PARCELABLE_BUNDLE_REQUEST_TASK_CALL_BACK:
+                        bundle = msg.getData();
+                        SparseArray<Bundle> sparseArray = bundle.getSparseParcelableArray(
+                            CALLBACK_SPARSE_PARCELABLE_BUNDLE);
+                        if (sparseArray == null) {
+                            Log.e(TAG, "[sparseArray] = null");
+                            return;
+                        }
+                        Bundle pathBundle = sparseArray.get(
+                            BUNDLE_SPARSE_PARCELABLE_BUNDLE_PATH_INDEX);
+                        ArrayList<String> pathList = pathBundle.getStringArrayList(
+                            BUNDLE_SPARSE_PARCELABLE_BUNDLE_ARRAY_LIST);
+                        Bundle idBundle = sparseArray.get(BUNDLE_SPARSE_PARCELABLE_BUNDLE_ID_INDEX);
+                        ArrayList<String> idList = idBundle.getStringArrayList(
+                            BUNDLE_SPARSE_PARCELABLE_BUNDLE_ARRAY_LIST);
+                        ToastUtils.show(MessengerActivity.this, "[pathList] = " +
+                                pathList + "  [idList] = " +
+                                idList,
+                            Toast.LENGTH_LONG);
                         break;
                 }
             }
-        };
-        this.serviceCallBackMessenger = new Messenger(mainHandler);
-
-        this.connection = new ServiceConnection() {
-            @Override public void onServiceConnected(ComponentName name, IBinder service) {
-                sendMessenger = new Messenger(service);
-                try {
-                    sendMessenger.send(createRequestMessage());
-                } catch (RemoteException e) {
-                    e.printStackTrace();
-                }
-            }
-
-
-            @Override public void onServiceDisconnected(ComponentName name) {
-
-            }
-        };
+        });
     }
 
 }
--- app/src/main/java/com/camnter/newlife/utils/ipc/MessengerService.java
+++ app/src/main/java/com/camnter/newlife/utils/ipc/MessengerService.java
@@ -11,6 +11,8 @@ import android.os.Messenger;
 import android.os.RemoteException;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.util.SparseArray;
+import java.util.ArrayList;
 
 /**
  * @author CaMnter
@@ -23,10 +25,29 @@ public class MessengerService extends Service {
     public static final int ENCODING_REQUEST_TASK = 0x261;
     public static final String BUNDLE_KEY_PATH = "bundle_key_path";
 
+    public static final int ARRAY_REQUEST_TASK = 0x271;
+    public static final String BUNDLE_ARRAY = "bundle_array";
+    public static final String BUNDLE_ARRAY_LIST = "bundle_array_list";
+
+    public static final int SPARSE_PARCELABLE_BUNDLE_TASK = 0x281;
+    public static final String BUNDLE_SPARSE_PARCELABLE_BUNDLE = "bundle_sparse_parcelable_array";
+    public static final int BUNDLE_SPARSE_PARCELABLE_BUNDLE_PATH_INDEX = 0;
+    public static final int BUNDLE_SPARSE_PARCELABLE_BUNDLE_ID_INDEX = 1;
+    public static final String BUNDLE_SPARSE_PARCELABLE_BUNDLE_ARRAY_LIST
+        = "bundle_sparse_parcelable_array_list";
+
+    // CallBack
+
     public static final int ENCODING_REQUEST_TASK_CALL_BACK = 0x262;
     public static final String BUNDLE_REQUEST_CODE = "bundle_request_code";
-    public static final int ENCODING_REQUEST_TASK_SUCCESS = 0x1;
-    public static final int ENCODING_REQUEST_TASK_FAILURE = 0x0;
+    public static final int ENCODING_REQUEST_TASK_SUCCESS = 0x11;
+
+    public static final int ARRAY_REQUEST_TASK_CALL_BACK = 0x272;
+    public static final String CALLBACK_BUNDLE_ARRAY = "callback_bundle_array";
+    public static final String CALLBACK_BUNDLE_ARRAY_LIST = "callback_bundle_array_list";
+
+    public static final int SPARSE_PARCELABLE_BUNDLE_REQUEST_TASK_CALL_BACK = 0x282;
+    public static final String CALLBACK_SPARSE_PARCELABLE_BUNDLE = "call_sparse_parcelable_array";
 
     private Messenger messenger;
     // 子进程内的主线程
@@ -37,19 +58,73 @@ public class MessengerService extends Service {
         super.onCreate();
         this.mainHandler = new Handler(Looper.getMainLooper()) {
             @Override public void handleMessage(Message msg) {
+                Messenger replyMessenger;
+                Bundle bundle;
                 switch (msg.what) {
                     case ENCODING_REQUEST_TASK:
-                        Messenger replyMessenger = msg.replyTo;
-                        Bundle bundle = msg.getData();
+                        replyMessenger = msg.replyTo;
+                        bundle = msg.getData();
                         String path = bundle.getString(BUNDLE_KEY_PATH);
                         Log.e(TAG, "[path] = " + path);
                         mainHandler.postDelayed(() -> {
                             Message replyMessage = Message.obtain(null,
                                 ENCODING_REQUEST_TASK_CALL_BACK);
-                            Log.e(TAG, "[postDelayed]  [replyMessage] = " + replyMessage);
-                            Log.e(TAG, "[postDelayed]  [replyMessenger] = " + replyMessenger);
                             Bundle replyBundle = new Bundle();
                             replyBundle.putInt(BUNDLE_REQUEST_CODE, ENCODING_REQUEST_TASK_SUCCESS);
+                            replyMessage.setData(replyBundle);
+                            try {
+                                replyMessenger.send(replyMessage);
+                            } catch (RemoteException e) {
+                                e.printStackTrace();
+                            }
+                        }, 3333);
+                        break;
+                    case ARRAY_REQUEST_TASK:
+                        replyMessenger = msg.replyTo;
+                        bundle = msg.getData();
+                        String[] stringArray = bundle.getStringArray(BUNDLE_ARRAY);
+                        ArrayList<String> stringList = bundle.getStringArrayList(
+                            BUNDLE_ARRAY_LIST);
+                        Log.e(TAG, "[stringArray] = " + stringArray.length);
+                        Log.e(TAG, "[stringList] = " + stringList);
+                        mainHandler.postDelayed(() -> {
+                            Message replyMessage = Message.obtain(null,
+                                ARRAY_REQUEST_TASK_CALL_BACK);
+                            Bundle replyBundle = new Bundle();
+                            replyBundle.putStringArray(CALLBACK_BUNDLE_ARRAY, stringArray);
+                            replyBundle.putStringArrayList(CALLBACK_BUNDLE_ARRAY_LIST, stringList);
+                            replyMessage.setData(replyBundle);
+                            try {
+                                replyMessenger.send(replyMessage);
+                            } catch (RemoteException e) {
+                                e.printStackTrace();
+                            }
+                        }, 3333);
+                        break;
+                    case SPARSE_PARCELABLE_BUNDLE_TASK:
+                        replyMessenger = msg.replyTo;
+                        bundle = msg.getData();
+                        SparseArray<Bundle> sparseArray = bundle.getSparseParcelableArray(
+                            BUNDLE_SPARSE_PARCELABLE_BUNDLE);
+                        if (sparseArray == null) {
+                            Log.e(TAG, "[sparseArray] = null");
+                            return;
+                        }
+                        Bundle pathBundle = sparseArray.get(
+                            BUNDLE_SPARSE_PARCELABLE_BUNDLE_PATH_INDEX);
+                        ArrayList<String> pathList = pathBundle.getStringArrayList(
+                            BUNDLE_SPARSE_PARCELABLE_BUNDLE_ARRAY_LIST);
+                        Bundle idBundle = sparseArray.get(BUNDLE_SPARSE_PARCELABLE_BUNDLE_ID_INDEX);
+                        ArrayList<String> idList = idBundle.getStringArrayList(
+                            BUNDLE_SPARSE_PARCELABLE_BUNDLE_ARRAY_LIST);
+                        Log.e(TAG, "[idList] = " + idList);
+                        Log.e(TAG, "[pathList] = " + pathList);
+                        mainHandler.postDelayed(() -> {
+                            Message replyMessage = Message.obtain(null,
+                                SPARSE_PARCELABLE_BUNDLE_REQUEST_TASK_CALL_BACK);
+                            Bundle replyBundle = new Bundle();
+                            replyBundle.putSparseParcelableArray(CALLBACK_SPARSE_PARCELABLE_BUNDLE,
+                                sparseArray);
                             replyMessage.setData(replyBundle);
                             try {
                                 replyMessenger.send(replyMessage);
