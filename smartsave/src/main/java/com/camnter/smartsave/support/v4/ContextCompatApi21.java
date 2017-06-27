3fde57bf402d7650aa008412b5d8346f980bd9a0
CaMnter <yuanyu.camnter@gmail.com>
2017-06-27 17:36:35 +0800

    [Add] ContextCompat in smartsave module

---
+++ smartsave/src/main/java/com/camnter/smartsave/support/v4/ContextCompatApi21.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.camnter.smartsave.support.v4;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import java.io.File;
+
+@TargetApi(21)
+class ContextCompatApi21 {
+    public static Drawable getDrawable(Context context, int id) {
+        return context.getDrawable(id);
+    }
+
+
+    public static File getNoBackupFilesDir(Context context) {
+        return context.getNoBackupFilesDir();
+    }
+
+
+    public static File getCodeCacheDir(Context context) {
+        return context.getCodeCacheDir();
+    }
+}
