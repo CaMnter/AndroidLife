d50143e8712393f81583bce8d30e62a0eabd38da
CaMnter <yuanyu.camnter@gmail.com>
2017-06-27 10:59:04 +0800

    [Add] use the SaveView and SaveOnClick annotation in app module

--- app/src/main/java/com/camnter/newlife/ui/activity/AsyncTaskActivity.java
+++ app/src/main/java/com/camnter/newlife/ui/activity/AsyncTaskActivity.java
@@ -2,23 +2,30 @@ package com.camnter.newlife.ui.activity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+import com.camnter.annotation.processor.annotation.SaveOnClick;
+import com.camnter.annotation.processor.annotation.SaveView;
 import com.camnter.newlife.R;
 import com.camnter.newlife.core.activity.BaseAppCompatActivity;
 import com.camnter.newlife.utils.asynctask.ProgressBarAsyncTask;
+import com.camnter.smartsave.SmartSave;
+import com.camnter.utils.wrapper.SmartToastWrapper;
 
 /**
  * Description：AsyncTaskActivity
  * Created by：CaMnter
  * Time：2015-09-17 14:12
  */
-public class AsyncTaskActivity extends BaseAppCompatActivity implements View.OnClickListener {
+public class AsyncTaskActivity extends BaseAppCompatActivity {
 
-    private ProgressBar progressBar;
-    private TextView textview;
-    private Button start;
+    @SaveView(R.id.textview)
+    TextView textview;
+    @SaveView(R.id.progressBar)
+    ProgressBar progressBar;
+
+    private SmartToastWrapper smartToastWrapper;
 
 
     /**
@@ -26,7 +33,8 @@ public class AsyncTaskActivity extends BaseAppCompatActivity implements View.OnC
      *
      * @return layout id
      */
-    @Override protected int getLayoutId() {
+    @Override
+    protected int getLayoutId() {
         return R.layout.activity_asynctask;
     }
 
@@ -36,42 +44,46 @@ public class AsyncTaskActivity extends BaseAppCompatActivity implements View.OnC
      *
      * @param savedInstanceState savedInstanceState
      */
-    @Override protected void initViews(Bundle savedInstanceState) {
-        this.textview = (TextView) this.findViewById(R.id.textview);
-        this.progressBar = (ProgressBar) this.findViewById(R.id.progressBar);
-        this.start = (Button) this.findViewById(R.id.start);
+    @Override
+    protected void initViews(Bundle savedInstanceState) {
+        SmartSave.save(this);
+        this.smartToastWrapper = new SmartToastWrapper() {
+            @Override
+            protected Toast getToast() {
+                return Toast.makeText(AsyncTaskActivity.this, "", Toast.LENGTH_LONG);
+            }
+        };
     }
 
 
     /**
      * Initialize the View of the listener
      */
-    @Override protected void initListeners() {
-        this.start.setOnClickListener(this);
+    @Override
+    protected void initListeners() {
     }
 
 
     /**
      * Initialize the Activity data
      */
-    @Override protected void initData() {
+    @Override
+    protected void initData() {
 
     }
 
 
-    /**
-     * Called when a view has been clicked.
-     *
-     * @param v The view that was clicked.
-     */
-    @Override public void onClick(View v) {
+    @SaveOnClick({ R.id.start, R.id.progressBar })
+    public void onSmartClick(View v) {
         switch (v.getId()) {
-            case R.id.start: {
+            case R.id.start:
                 ProgressBarAsyncTask asyncTask = new ProgressBarAsyncTask(this.progressBar,
                     this.textview);
                 asyncTask.execute("%");
                 break;
-            }
+            case R.id.progressBar:
+                this.smartToastWrapper.show(this.progressBar.getProgress() + "%");
+                break;
         }
     }
 
