a7428edf789f95e3ece3d12fe5e62ecb58ac392d
CaMnter <yuanyu.camnter@gmail.com>
2017-07-23 22:20:28 +0800

    [Add] SmartRouters in smartrouter module

--- smartrouter/src/main/java/com/camnter/smartrouter/SmartRouters.java
+++ smartrouter/src/main/java/com/camnter/smartrouter/SmartRouters.java
@@ -3,7 +3,14 @@ package com.camnter.smartrouter;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import com.camnter.smartrouter.core.Filter;
+import com.camnter.smartrouter.core.Router;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author CaMnter
@@ -11,38 +18,190 @@ import android.support.annotation.NonNull;
 
 public final class SmartRouters {
 
-    private static final String scheme = "routers";
+    private static final Map<String, Router> ROUTER_MAP = new HashMap<>();
+    private static final Map<String, Class<? extends Activity>> ACTIVITY_CLASS_MAP
+        = new HashMap<>();
+    private static String SCHEME = "routers";
+    private static String HOST = "";
+    private static Filter FILTER;
 
 
     public static String getScheme() {
-        return scheme;
+        return SCHEME;
     }
 
 
-    public static void start(@NonNull final Context context,
-                             @NonNull final String url) {
-        // TODO
+    public static void setScheme(@NonNull final String scheme) {
+        SCHEME = scheme;
     }
 
 
-    public static void startForResult(@NonNull final Activity activity,
-                                      @NonNull final String url,
-                                      final int requestCode) {
-        // TODO
+    public static String getHost() {
+        return HOST;
     }
 
 
-    public static void startForResult(@NonNull final Fragment fragment,
-                                      @NonNull final String url,
-                                      final int requestCode) {
-        // TODO
+    public static void setHost(String host) {
+        HOST = host;
     }
 
 
-    public static void startForResult(@NonNull final android.support.v4.app.Fragment fragment,
-                                      @NonNull final String url,
-                                      final int requestCode) {
-        // TODO
+    public static Filter getFilter() {
+        return FILTER;
+    }
+
+
+    public static void setgetFilter(Filter getFilter) {
+        FILTER = getFilter;
+    }
+
+
+    private static void running(@NonNull final Activity activity) {
+        final String targetFullName = activity.getClass().getName();
+        try {
+            Router router = ROUTER_MAP.get(targetFullName);
+            if (router == null) {
+                Class<?> routerClass = Class.forName(targetFullName + "_Router");
+                router = (Router) routerClass.newInstance();
+                ROUTER_MAP.put(targetFullName, router);
+            }
+            router.setFieldValue(activity);
+        } catch (Exception e) {
+            new Throwable("[SmartRouters]   [running]   " + targetFullName, e).printStackTrace();
+        }
+    }
+
+
+    public static void register(@NonNull final Router register) {
+        register.register(ACTIVITY_CLASS_MAP);
+    }
+
+
+    private static Class<? extends Activity> getActivityClass(@NonNull final String url,
+                                                              @NonNull final Uri uri) {
+        final int index = url.indexOf('?');
+        // scheme:host?
+        final String schemeAndHost = index > 0 ? url.substring(0, index) : url;
+        Class<? extends Activity> clazz = ACTIVITY_CLASS_MAP.get(schemeAndHost);
+        if (clazz != null) {
+            return clazz;
+        }
+        String host;
+        if (SCHEME.equals(uri.getScheme())) {
+            host = uri.getHost();
+            return ACTIVITY_CLASS_MAP.get(host);
+        }
+        return null;
+    }
+
+
+    public static boolean start(@NonNull final Context context,
+                                @NonNull final String url) {
+        if (TextUtils.isEmpty(url)) {
+            return false;
+        }
+        if (FILTER != null) {
+            final String mapUrl = FILTER.map(url);
+            if (FILTER.start(context, mapUrl)) {
+                return false;
+            }
+        }
+
+        final Uri uri = Uri.parse(url);
+        Class clazz = getActivityClass(url, uri);
+        if (clazz == null) {
+            new Throwable(url + "can't start").printStackTrace();
+            return false;
+        }
+
+        Intent intent = new Intent(context, clazz);
+        intent.setData(uri);
+        if (!(context instanceof Activity)) {
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        }
+        context.startActivity(intent);
+        return true;
+    }
+
+
+    public static boolean startForResult(@NonNull final Activity activity,
+                                         @NonNull final String url,
+                                         final int requestCode) {
+        if (TextUtils.isEmpty(url)) {
+            return false;
+        }
+        if (FILTER != null) {
+            final String mapUrl = FILTER.map(url);
+            if (FILTER.startForResult(activity, mapUrl, requestCode)) {
+                return false;
+            }
+        }
+
+        final Uri uri = Uri.parse(url);
+        Class clazz = getActivityClass(url, uri);
+        if (clazz == null) {
+            new Throwable(url + "can't startForResult").printStackTrace();
+            return false;
+        }
+
+        Intent intent = new Intent(activity, clazz);
+        intent.setData(uri);
+        activity.startActivityForResult(intent, requestCode);
+        return true;
+    }
+
+
+    public static boolean startForResult(@NonNull final Fragment fragment,
+                                         @NonNull final String url,
+                                         final int requestCode) {
+        if (TextUtils.isEmpty(url)) {
+            return false;
+        }
+        if (FILTER != null) {
+            final String mapUrl = FILTER.map(url);
+            if (FILTER.startForResult(fragment, mapUrl, requestCode)) {
+                return false;
+            }
+        }
+
+        final Uri uri = Uri.parse(url);
+        Class clazz = getActivityClass(url, uri);
+        if (clazz == null) {
+            new Throwable(url + "can't startForResult").printStackTrace();
+            return false;
+        }
+
+        Intent intent = new Intent(fragment.getActivity(), clazz);
+        intent.setData(uri);
+        fragment.startActivityForResult(intent, requestCode);
+        return true;
+    }
+
+
+    public static boolean startForResult(@NonNull final android.support.v4.app.Fragment fragment,
+                                         @NonNull final String url,
+                                         final int requestCode) {
+        if (TextUtils.isEmpty(url)) {
+            return false;
+        }
+        if (FILTER != null) {
+            final String mapUrl = FILTER.map(url);
+            if (FILTER.startForResult(fragment, mapUrl, requestCode)) {
+                return false;
+            }
+        }
+
+        final Uri uri = Uri.parse(url);
+        Class clazz = getActivityClass(url, uri);
+        if (clazz == null) {
+            new Throwable(url + "can't startForResult").printStackTrace();
+            return false;
+        }
+
+        Intent intent = new Intent(fragment.getActivity(), clazz);
+        intent.setData(uri);
+        fragment.startActivityForResult(intent, requestCode);
+        return true;
     }
 
 }
