6edd891af0bc4c1272d06a69b3114bd40ac90dff
CaMnter <yuanyu.camnter@gmail.com>
2017-09-10 21:51:53 +0800

    [Update] ButterKnifePlugin plugin in butterknife-gradle-plugin module

--- butterknife-8.6.0/butterknife-gradle-plugin/src/main/java/butterknife/plugin/ButterKnifePlugin.kt
+++ butterknife-8.6.0/butterknife-gradle-plugin/src/main/java/butterknife/plugin/ButterKnifePlugin.kt
@@ -1,15 +1,13 @@
-package butterknife.plugin
+package butterknife.plugin;
 
-import com.android.build.gradle.AppExtension
-import com.android.build.gradle.AppPlugin
-import com.android.build.gradle.LibraryExtension
-import com.android.build.gradle.LibraryPlugin
+import com.android.build.gradle.*
 import com.android.build.gradle.api.BaseVariant
 import org.gradle.api.DomainObjectSet
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.ExtensionContainer
 import java.io.File
+import java.util.concurrent.atomic.AtomicBoolean
 import kotlin.reflect.KClass
 
 class ButterKnifePlugin : Plugin<Project> {
@@ -17,39 +15,88 @@ class ButterKnifePlugin : Plugin<Project> {
         project.plugins.all {
             when (it) {
             /*
-             * instanceof LibraryPlugin
+             * instance of FeaturePlugin
+             * project.extensions >> ExtensionContainer
+             * fun <T : Any> ExtensionContainer.get(type: KClass<T>): T >> FeatureExtension
+             *
+             * FeaturePlugin extends LibraryPlugin
+             *
+             * FeaturePlugin.featureVariants >> DomainObjectSet<ApplicationVariant>
+             * FeaturePlugin.libraryVariants >> DomainObjectSet<ApplicationVariant>
+             */
+                is FeaturePlugin -> {
+                    project.extensions[FeatureExtension::class].run {
+                        configureR2Generation(project, featureVariants)
+                        configureR2Generation(project, libraryVariants)
+                    }
+                }
+            /*
+             * instance of LibraryPlugin
              * project.extensions >> ExtensionContainer
              * fun <T : Any> ExtensionContainer.get(type: KClass<T>): T >> LibraryExtension
              * LibraryExtension.libraryVariants >> DefaultDomainObjectSet<LibraryVariant>
              */
-                is LibraryPlugin -> applyPlugin(project.extensions[LibraryExtension::class].libraryVariants)
+                is LibraryPlugin -> {
+                    project.extensions[LibraryExtension::class].run {
+                        configureR2Generation(project, libraryVariants)
+                    }
+                }
             /*
-             * instanceof AppPlugin
+             * instance of AppPlugin
              * project.extensions >> ExtensionContainer
              * fun <T : Any> ExtensionContainer.get(type: KClass<T>): T >> AppExtension
-             * AppExtension.libraryVariants >> DomainObjectSet<ApplicationVariant>
+             * AppExtension.applicationVariants >> DomainObjectSet<ApplicationVariant>
              */
-                is AppPlugin -> applyPlugin(project.extensions[AppExtension::class].applicationVariants)
+                is AppPlugin -> {
+                    project.extensions[AppExtension::class].run {
+                        configureR2Generation(project, applicationVariants)
+                    }
+                }
             }
         }
     }
 
-    private fun applyPlugin(variants: DomainObjectSet<out BaseVariant>) {
+    private fun configureR2Generation(project: Project, variants: DomainObjectSet<out BaseVariant>) {
+        // 遍历 DomainObjectSet<out BaseVariant>
         variants.all { variant ->
-            // 遍历 List<BaseVariantOutput>
-            variant.outputs.forEach { output ->
+            // 获取每个 output 文件夹 File
+            val outputDir = project.buildDir.resolve(
+                    "generated/source/r2/${variant.dirName}")
+
+            // 创建对应的 R2 任务
+            val task = project.tasks.create("generate${variant.name.capitalize()}R2")
+            // 设置 R2 任务的 输出目录 File
+            task.outputs.dir(outputDir)
+            // 注册任务
+            variant.registerJavaGeneratingTask(task, outputDir)
+
+            val once = AtomicBoolean()
+
+            // 遍历 DomainObjectCollection<BaseVariantOutput>
+            variant.outputs.all { output ->
                 // ProcessAndroidResources
                 val processResources = output.processResources
-                // TODO proper task registered as source-generating?
-                // 在 processResources 阶段添加任务
-                processResources.doLast {
-                    // 拿到 R 文件路径，替换分隔符
-                    val pathToR = processResources.packageForR.replace('.', File.separatorChar)
+                // ProcessAndroidResources 添加到任务内
+                task.dependsOn(processResources)
+
+                // Though there might be multiple outputs, their R files are all the same. Thus, we only
+                // need to configure the task once with the R.java input and action.
+                if (once.compareAndSet(false, true)) {
+                    // 拿到 R 文件夹路径
+                    val rPackage = processResources.packageForR
+                    // 替换 R 文件夹路径 的 分隔符
+                    val pathToR = rPackage.replace('.', File.separatorChar)
                     // 拿到 R 文件
                     val rFile = processResources.sourceOutputDir.resolve(pathToR).resolve("R.java")
-                    // JavaPoet + JavaParser 生成 R2.java
-                    FinalRClassBuilder.brewJava(rFile, processResources.sourceOutputDir,
-                            processResources.packageForR, "R2")
+
+                    task.apply {
+                        // 注册 R File 到任务内
+                        inputs.file(rFile)
+                        // JavaPoet + JavaParser 生成 R2.java
+                        doLast {
+                            FinalRClassBuilder.brewJava(rFile, outputDir, rPackage, "R2")
+                        }
+                    }
                 }
             }
         }
