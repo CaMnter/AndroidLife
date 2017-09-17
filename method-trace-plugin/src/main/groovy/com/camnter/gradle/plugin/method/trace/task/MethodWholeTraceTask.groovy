3e7ce7c7c29f116a98e3e880b3d290a9284c2622
CaMnter <yuanyu.camnter@gmail.com>
2017-09-17 23:23:19 +0800

    [Fix]  MethodWholeTraceTask not output

--- method-trace-plugin/src/main/groovy/com/camnter/gradle/plugin/method/trace/task/MethodWholeTraceTask.groovy
+++ method-trace-plugin/src/main/groovy/com/camnter/gradle/plugin/method/trace/task/MethodWholeTraceTask.groovy
@@ -2,18 +2,14 @@ package com.camnter.gradle.plugin.method.trace.task
 
 import com.android.build.gradle.AppExtension
 import com.camnter.gradle.plugin.method.trace.utils.FileUtils
-import org.gradle.api.Action
 import org.gradle.api.DefaultTask
-import org.gradle.api.Task
-
-/**
- * @author CaMnter
- */
+import org.gradle.api.tasks.TaskAction
 
 class MethodWholeTraceTask extends DefaultTask {
 
-    @Override
-    Task doLast(Action<? super Task> action) {
+    @TaskAction
+    void main() {
+        println '[MethodWholeTraceTask]   [main]'
         def capturesDirFile = new File(
                 project.getRootProject().projectDir.path + FileUtils.FILE_SEPARATOR + 'captures')
         capturesDirFile.traverse {
@@ -25,8 +21,8 @@ class MethodWholeTraceTask extends DefaultTask {
             txtFile.write('')
 
             // DmTraceDump command
-            def command = "${platformToolsPath}dmtracedump  -ho ${it.absolutePath} >> ${txtFile.absolutePath}"
-            println "[WholeMethodTraceTask]   [doLast]   [command] == ${command}"
+            def command = "${platformToolsPath}/dmtracedump  -ho ${it.absolutePath} >> ${txtFile.absolutePath}"
+            println "[MethodWholeTraceTask]   [main]   [command] == ${command}"
 
             def osName = System.getProperty("os.name").toLowerCase()
 
@@ -38,17 +34,15 @@ class MethodWholeTraceTask extends DefaultTask {
                 ['bash', '-c', command].execute()
             }
         }
-        return super.doLast(action)
     }
 
     def getPlatformToolsPath() {
         def localPropertiesFile = new File(project.rootDir, 'local.properties')
 
         def sdkPath = null
-        if (localProperties.exists()) {
+        if (localPropertiesFile.exists()) {
             def properties = new Properties()
-            localPropertiesFile.withInputStream { inputStream -> properties.load(inputStream)
-            }
+            localPropertiesFile.withInputStream { inputStream -> properties.load(inputStream) }
             sdkPath = properties.getProperty('sdk.dir')
         }
         if (null == sdkPath || sdkPath == '') {
