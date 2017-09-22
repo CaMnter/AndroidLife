3c50598532ed3d6c02eb799dab15c92b7c75899c
CaMnter <yuanyu.camnter@gmail.com>
2017-09-22 18:26:10 +0800

    [Add] method-trace-plugin 1.1.1 version

--- gradle-plugin-life/method-trace-plugin/src/main/groovy/com/camnter/gradle/plugin/method/trace/task/MethodExpectTraceTask.groovy
+++ gradle-plugin-life/method-trace-plugin/src/main/groovy/com/camnter/gradle/plugin/method/trace/task/MethodExpectTraceTask.groovy
@@ -52,30 +52,70 @@ class MethodExpectTraceTask extends DefaultTask {
                 expectEnterFile.write('')
                 expectEnterExitFile.write('')
 
+                Stack<String> entTrace = new Stack<>()
                 it.eachLine { line ->
                     /**
-                     * 仅输出 ent
+                     * 输出 ent
                      * 有的 traceview  包含 方法签名
                      * 有的 traceview  包含 package name
                      * */
-                    if (line.contains(' ent ') && line.contains(packageName) ||
-                            line.contains(packageNameSignature)) {
+                    if (line.contains(' ent ') && (line.contains(packageName) || line.contains(
+                            packageNameSignature))) {
+                        entTrace.push(line)
                         expectEnterFile.append("${line}\n")
-                    }
-                    /**
-                     * 输出 ent 和 xit
-                     * 有的 traceview  包含 方法签名
-                     * 有的 traceview  包含 package name
-                     * */
-                    if (line.contains(packageName) || line.contains(packageNameSignature)) {
                         expectEnterExitFile.append("${line}\n")
                     }
+                    /**
+                     * xit
+                     * 有的 traceview  包含 方法签名
+                     * 有的 traceview  包含 package name
+                     * */
+                    if (line.contains(' xit ') && (line.contains(packageName) || line.contains(
+                            packageNameSignature))) {
+                        expectEnterExitFile.append("${line}\n")
+                        def entLine = entTrace.peek()
+                        if (line.substring(line.indexOf('.')) == entLine.substring(
+                                entLine.indexOf('.'))) {
+                            entTrace.pop()
+                            def xitTime = getTimeFromTraceInfo(TraceType.Xit, line)
+                            def entTime = getTimeFromTraceInfo(TraceType.Ent, entLine)
+                            def elapsedTime = xitTime - entTime
+                            def elapsedTimeString = elapsedTime as String
+                            expectEnterExitFile.append("${'diff ---  ' + elapsedTimeString}\n")
+                        }
+                    }
                 }
-                printf "%-60s  =  %s",
+                printf "%60s  =  %s",
                         ["[MethodExpectTraceTask]   [main]   [expectEnterFile]", "${expectEnterFile.path}\n"]
-                printf "%-60s  =  %s",
+                printf "%60s  =  %s",
                         ["[MethodExpectTraceTask]   [main]   [expectEnterExitFile]", "${expectEnterExitFile.path}\n"]
             }
+        }
+    }
+
+    long getTimeFromTraceInfo(TraceType traceType, String trace) {
+        def firstPointIndex = trace.indexOf('.')
+        def firstHalfContent = trace.substring(0, firstPointIndex).trim()
+        def time = firstHalfContent.substring(
+                firstHalfContent.indexOf(traceType.value) + traceType.value.length())
+        if (time.isLong()) {
+            return time as Long
+        }
+        return 0
+    }
+
+    enum TraceType {
+        Ent('ent'),
+        Xit('xit')
+
+        private final String value
+
+        private TraceType(String value) {
+            this.value = value
+        }
+
+        String toString() {
+            return this.value
         }
     }
 }
