b512aaf73860cf713840d3957582e8ce7dacccc4
CaMnter <yuanyu.camnter@gmail.com>
2017-09-29 21:00:38 +0800

    [Add] FileUtils and ImageUtils in life-kit module

---
+++ gradle-plugin-life/life-kit/src/main/groovy/com/camnter/gradle/plugin/life/kit/ImageUtils.groovy
@@ -0,0 +1,48 @@
+package com.camnter.gradle.plugin.life.kit
+
+import javax.imageio.ImageIO
+import java.awt.image.BufferedImage
+
+/**
+ * @author CaMnter
+ */
+
+class ImageUtils {
+
+    public static final String JPG = ".jpg"
+    public static final String JPEG = ".jpeg"
+    public static final String PNG = ".png"
+    public static final String DOT_9PNG = ".9.png"
+
+    static boolean checkImage(File file) {
+        return (file.getName().contains(JPG) || file.getName().contains(PNG) ||
+                file.getName().contains(JPEG)) && !file.getName().contains(DOT_9PNG)
+    }
+
+    static boolean checkJPG(File file) {
+        return file.getName().contains(JPG) || file.getName().endsWith(JPEG)
+    }
+
+    static boolean checkAlphaPNG(File filePath) {
+        if (filePath.exists()) {
+            try {
+                BufferedImage bufferedImage = ImageIO.read(filePath)
+                return bufferedImage.getColorModel().hasAlpha()
+            } catch (Exception e) {
+                println e.message
+                return false
+            }
+        } else {
+            return false
+        }
+    }
+
+    static boolean checkImageSize(File imageFile, int maxSize) {
+        if (checkImage(imageFile)) {
+            if (imageFile.length() >= maxSize) {
+                return true
+            }
+        }
+        return false
+    }
+}
