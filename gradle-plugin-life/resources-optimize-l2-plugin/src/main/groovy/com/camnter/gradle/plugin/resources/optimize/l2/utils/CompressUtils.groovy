47a18bed4c05fdca14029ee90fd91b2912623c15
CaMnter <yuanyu.camnter@gmail.com>
2017-10-14 20:09:28 +0800

    [Add] CompressUtils in resources-optimize-l2-plugin module

---
+++ gradle-plugin-life/resources-optimize-l2-plugin/src/main/groovy/com/camnter/gradle/plugin/resources/optimize/l2/utils/CompressUtils.groovy
@@ -0,0 +1,32 @@
+package com.camnter.gradle.plugin.resources.optimize.l2.utils
+
+/**
+ * @author CaMnter
+ */
+
+class CompressUtils {
+
+    static final TAG = 'CompressUtils'
+
+    static void compressResource(File file, Closure pngClosure, Closure jpgClosure) {
+
+        if (ImageUtils.checkImage(file)) {
+            def originalSize = file.length()
+            def path = file.path
+            if (ImageUtils.checkJPG(file)) {
+                /**
+                 * eg: "guetzli ${file.path} ${file.path}"
+                 * */
+                jpgClosure.call(file)
+            } else {
+                /**
+                 * eg: "pngquant --skip-if-larger --speed 3 --force --output ${file.path} -- ${file.path}"
+                 * */
+                pngClosure.call(file)
+            }
+            def currentSize = file.length()
+            printf "%-85s  %s\n",
+                    ["[${TAG}]   [originalSize] = ${originalSize}   [currentSize] = ${currentSize}", path]
+        }
+    }
+}
