1dbe81786f2d055695af3b4be239ede69f17ff23
CaMnter <yuanyu.camnter@gmail.com>
2017-10-14 19:43:15 +0800

    [Add] AndroidUtils and PluginUtils in resources-optimize-l2-plugin module

---
+++ gradle-plugin-life/resources-optimize-l2-plugin/src/main/groovy/com/camnter/gradle/plugin/resources/optimize/l2/utils/AndroidUtils.groovy
@@ -0,0 +1,25 @@
+package com.camnter.gradle.plugin.resources.optimize.l2.utils
+
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.FeatureExtension
+import com.android.build.gradle.LibraryExtension
+import org.gradle.api.Project
+
+/**
+ * @author CaMnter
+ */
+
+class AndroidUtils {
+
+    static int getMinSdkVersion(Project project) {
+        def minSdkVersion = 0
+        PluginUtils.dispatchPlugin(project) { AppExtension appExtension ->
+            minSdkVersion = appExtension.defaultConfig.minSdkVersion.apiLevel
+        } { LibraryExtension libraryExtension ->
+            minSdkVersion = libraryExtension.defaultConfig.minSdkVersion.apiLevel
+        } { FeatureExtension featureExtension ->
+            minSdkVersion = featureExtension.defaultConfig.minSdkVersion.apiLevel
+        }
+        return minSdkVersion
+    }
+}
---
+++ gradle-plugin-life/resources-optimize-l2-plugin/src/main/groovy/com/camnter/gradle/plugin/resources/optimize/l2/utils/PluginUtils.groovy
@@ -0,0 +1,49 @@
+package com.camnter.gradle.plugin.resources.optimize.l2.utils
+
+import com.android.build.gradle.*
+import org.gradle.api.Project
+
+/**
+ * @author CaMnter
+ */
+
+class PluginUtils {
+
+    static int dispatchPlugin(Project project, Closure<AppExtension> appClosure,
+            Closure<LibraryExtension> libraryClosure,
+            Closure<FeatureExtension> featureClosure) {
+        project.plugins.all {
+            if (it instanceof AppPlugin) {
+                AppExtension appExtension = project.extensions.getByType(AppExtension.class)
+                appClosure.call(appExtension)
+            } else if (it instanceof LibraryPlugin) {
+                LibraryExtension libraryExtension = project.extensions.getByType(
+                        LibraryExtension.class)
+                libraryClosure.call(libraryExtension)
+            } else if (it instanceof FeaturePlugin) {
+                FeatureExtension featureExtension = project.extensions.getByType(
+                        FeatureExtension.class)
+                featureClosure.call(featureExtension)
+            }
+        }
+    }
+
+    static void dispatchSystem(Closure osXClosure,
+            Closure linuxClosure,
+            Closure windowClosure) {
+        String system = System.getProperty("os.name")
+        switch (system) {
+            case "Mac OS X":
+                osXClosure.call()
+                break
+            case "Linux":
+                linuxClosure.call()
+                break
+            case "Windows":
+                windowClosure.call()
+                break
+            default:
+                return
+        }
+    }
+}
