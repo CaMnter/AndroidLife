3f5f0efd9f981f99fc8bfbe8ae65547261b21308
CaMnter <yuanyu.camnter@gmail.com>
2017-12-04 19:46:48 +0800

    [Remove] some tasks in dex-method-counts-plugin module

--- dex-method-counts-plugin/src/main/groovy/com/camnter/gradle/plugin/dex/method/counts/DexMethodCountsPlugin.groovy
+++ dex-method-counts-plugin/src/main/groovy/com/camnter/gradle/plugin/dex/method/counts/DexMethodCountsPlugin.groovy
@@ -2,9 +2,6 @@ package com.camnter.gradle.plugin.dex.method.counts
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task
-import org.gradle.api.plugins.JavaPlugin
-import org.gradle.api.plugins.PluginContainer
 
 /**
  * @author CaMnter
@@ -13,22 +10,5 @@ import org.gradle.api.plugins.PluginContainer
 class DexMethodCountsPlugin implements Plugin<Project> {
 
     @Override
-    void apply(Project project) {
-        applyPluginIfNotApply(project, JavaPlugin.class)
-        Task dexMethodCountsBeforeJarTask = project.task('dexMethodCountsBeforeJar',
-                type: BeforeJarTask)
-        Task dexMethodCountsAfterJarTask = project.task('dexMethodCountsAfterJarTask',
-                type: AfterJarTask)
-        Task jarTask = project.tasks.getByName('jar')
-        jarTask.dependsOn dexMethodCountsBeforeJarTask
-        dexMethodCountsAfterJarTask.dependsOn jarTask
-    }
-
-    static <T extends Plugin> T applyPluginIfNotApply(Project project, Class<T> pluginClass) {
-        PluginContainer pluginContainer = project.getPlugins()
-        if (!pluginContainer.hasPlugin(pluginClass)) {
-            pluginContainer.apply(pluginClass)
-        }
-        return pluginContainer.getPlugin(pluginClass) as T
-    }
+    void apply(Project project) {}
 }
