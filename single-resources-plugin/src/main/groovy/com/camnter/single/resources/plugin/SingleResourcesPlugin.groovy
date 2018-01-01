0ed0135748685044ac2a4c6f5c14a00690e4a068
CaMnter <yuanyu.camnter@gmail.com>
2018-01-01 21:46:28 +0800

    [Add] single-resources-plugin module

---
+++ single-resources-plugin/src/main/groovy/com/camnter/single/resources/plugin/SingleResourcesPlugin.groovy
@@ -0,0 +1,13 @@
+package com.camnter.single.resources.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class SingleResourcesPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+
+}
