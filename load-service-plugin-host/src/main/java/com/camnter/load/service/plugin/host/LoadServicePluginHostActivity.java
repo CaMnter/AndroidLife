9a7bfed00456a2d0d65d7194903e829495fdc1ba
CaMnter <yuanyu.camnter@gmail.com>
2018-01-05 19:08:08 +0800

    [Modify] message of plugin service in load-service-host  module

--- load-service-plugin-host/src/main/java/com/camnter/load/service/plugin/host/LoadServicePluginHostActivity.java
+++ load-service-plugin-host/src/main/java/com/camnter/load/service/plugin/host/LoadServicePluginHostActivity.java
@@ -1,9 +1,16 @@
 package com.camnter.load.service.plugin.host;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.Looper;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.view.View;
 import android.widget.Toast;
 
@@ -13,7 +20,12 @@ import android.widget.Toast;
 
 public class LoadServicePluginHostActivity extends BaseAppCompatActivity {
 
-    final Handler handler = new Handler(Looper.getMainLooper());
+    private static final int ACTIVITY_MESSAGE = 0x2331;
+    private static final int PLUGIN_SERVICE_MESSAGE = 0x2332;
+
+    private Messenger serviceMessenger;
+    private Messenger activityMessenger;
+    private ServiceConnection connection;
 
     View startText;
 
@@ -41,30 +53,77 @@ public class LoadServicePluginHostActivity extends BaseAppCompatActivity {
             @Override
             public void onClick(View v) {
                 startText.setEnabled(false);
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            // plugin activity
-                            final SmartApplication smartApplication
-                                = (SmartApplication) LoadServicePluginHostActivity.this.getApplication();
-                            final Intent intent = new Intent(LoadServicePluginHostActivity.this,
-                                smartApplication
-                                    .getDexClassLoader()
-                                    .loadClass(
-                                        "com.camnter.load.service.plugin.plugin.PluginService"));
-                            startActivity(intent);
-                        } catch (Exception e) {
-                            ToastUtils.show(LoadServicePluginHostActivity.this, e.getMessage(),
-                                Toast.LENGTH_LONG);
-                            e.printStackTrace();
-                        } finally {
-                            startText.setEnabled(true);
-                        }
-                    }
-                });
+                requestUploadService();
             }
         });
+    }
+
+
+    private void requestUploadService() {
+        try {
+            if (this.connection == null) {
+                this.initCallbackMessenger();
+                this.connection = new ServiceConnection() {
+                    @Override
+                    public void onServiceConnected(ComponentName name, IBinder service) {
+                        serviceMessenger = new Messenger(service);
+                        try {
+                            serviceMessenger.send(createRequestMessage());
+                        } catch (RemoteException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+
+                    @Override
+                    public void onServiceDisconnected(ComponentName name) {
+                        connection = null;
+                    }
+                };
+                final Intent intent = new Intent(this,
+                    Class.forName("com.camnter.load.service.plugin.plugin.PluginService"));
+                intent.setAction("com.camnter.load.service.plugin");
+                this.bindService(intent, this.connection, Context.BIND_AUTO_CREATE);
+            } else {
+                try {
+                    serviceMessenger.send(this.createRequestMessage());
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private Message createRequestMessage() {
+        Message message = Message.obtain(null, ACTIVITY_MESSAGE);
+        final Bundle bundle = new Bundle();
+        bundle.putString("ACTIVITY_INFO", "There is Host Activity");
+
+        message.setData(bundle);
+        message.replyTo = this.activityMessenger;
+        return message;
+    }
+
+
+    private void initCallbackMessenger() {
+        this.activityMessenger = new Messenger(new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                startText.setEnabled(true);
+                final Bundle bundle = msg.getData();
+                final String info = bundle == null ? null : bundle.getString("SERVICE_INFO");
+                switch (msg.what) {
+                    case PLUGIN_SERVICE_MESSAGE:
+                        ToastUtils.show(LoadServicePluginHostActivity.this, String.valueOf(info),
+                            Toast.LENGTH_LONG);
+                        break;
+                }
+            }
+        });
+
     }
 
 
@@ -83,6 +142,15 @@ public class LoadServicePluginHostActivity extends BaseAppCompatActivity {
     @Override
     protected void initData() {
 
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        if (this.connection != null) {
+            this.unbindService(this.connection);
+        }
+        super.onDestroy();
     }
 
 }
