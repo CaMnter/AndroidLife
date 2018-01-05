2700a559680899d1fac57c9c1cc15f8d1c72d416
CaMnter <yuanyu.camnter@gmail.com>
2018-01-05 11:07:03 +0800

    [Add] load-service-plugin-plugin module

---
+++ load-service-plugin-plugin/src/main/java/com/camnter/load/service/plugin/plugin/PluginService.java
@@ -0,0 +1,89 @@
+package com.camnter.load.service.plugin.plugin;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.support.annotation.Nullable;
+
+/**
+ * @author CaMnter
+ */
+
+public class PluginService extends Service {
+
+    private static final int ACTIVITY_MESSAGE = 0x2331;
+    private static final int PLUGIN_SERVICE_MESSAGE = 0x2332;
+
+    private Messenger serviceMessenger;
+    private Messenger activityMessenger;
+
+    private HandlerThread handlerThread;
+
+
+    /**
+     * Called by the system when the service is first created.  Do not call this method directly.
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        this.handlerThread = new HandlerThread(PluginService.class.getName() + " # HandlerThread");
+        this.handlerThread.start();
+
+        final Handler handler = new Handler(this.handlerThread.getLooper()) {
+            /**
+             * Subclasses must implement this to receive messages.
+             */
+            @Override
+            public void handleMessage(Message msg) {
+                switch (msg.what) {
+                    case ACTIVITY_MESSAGE:
+                        activityMessenger = msg.replyTo;
+                        final Message message = Message.obtain(null, PLUGIN_SERVICE_MESSAGE);
+                        message.obj = "Plugin service response info";
+                        try {
+                            activityMessenger.send(message);
+                        } catch (RemoteException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                }
+            }
+        };
+        this.serviceMessenger = new Messenger(handler);
+    }
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return this.serviceMessenger.getBinder();
+    }
+
+
+    /**
+     * Called by the system to notify a Service that it is no longer used and is being removed.  The
+     * service should clean up any resources it holds (threads, registered
+     * receivers, etc) at this point.  Upon return, there will be no more calls
+     * in to this Service object and it is effectively dead.  Do not call this method directly.
+     */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                this.handlerThread.quitSafely();
+            } else {
+                this.handlerThread.quit();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
