6427bb3952c9c7237ae84ab3e0b1e51996966200
CaMnter <yuanyu.camnter@gmail.com>
2018-01-05 16:18:56 +0800

    [Add] load-service-plugin-host module

---
+++ load-service-plugin-host/src/main/java/com/camnter/load/service/plugin/host/AssetsUtils.java
@@ -0,0 +1,41 @@
+package com.camnter.load.service.plugin.host;
+
+import android.content.Context;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author CaMnter
+ */
+
+public class AssetsUtils {
+
+    private static final int BUF_SIZE = 2048;
+
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public static void copyAssets(Context context, String assetsName, String destFilePath)
+        throws IOException {
+        File file = new File(destFilePath);
+        if (!file.getParentFile().exists()) {
+            file.mkdirs();
+        }
+        if (file.exists()) {
+            file.delete();
+        }
+        FileOutputStream out = new FileOutputStream(file);
+
+        InputStream in = context.getAssets().open(assetsName);
+
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = in.read(buf)) != -1) {
+            out.write(buf, 0, len);
+        }
+        in.close();
+        out.close();
+    }
+
+}
