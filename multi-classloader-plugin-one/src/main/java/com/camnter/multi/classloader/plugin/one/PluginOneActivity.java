fcd7493c70d0e59b26ef1ba609c5e0ade009f3da
CaMnter <yuanyu.camnter@gmail.com>
2018-01-08 16:44:16 +0800

    [Add] multi-classloader-plugin-one module

---
+++ multi-classloader-plugin-one/src/main/java/com/camnter/multi/classloader/plugin/one/PluginOneActivity.java
@@ -0,0 +1,31 @@
+package com.camnter.multi.classloader.plugin.one;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * @author CaMnter
+ */
+
+public class PluginOneActivity extends AppCompatActivity {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final LinearLayout rootLayout = new LinearLayout(this);
+        rootLayout.setBackgroundColor(0xffffffff);
+        final TextView text = new TextView(this);
+        text.setTextColor(0xff000000);
+        text.setText("Plugin one activity");
+        text.setPadding(15, 15, 15, 15);
+        rootLayout.addView(text);
+        this.setContentView(rootLayout);
+    }
+
+}
