5fdc8ef903a308cc07f47c3fe0ac7e2cc90cde8f
CaMnter <yuanyu.camnter@gmail.com>
2018-01-17 17:56:34 +0800

    [Add] buildSrc module, and transplant the r2-plugin module to buildSrc module

---
+++ buildSrc/src/main/groovy/com/camnter/gradle/plugin/r2/PluginUtils.groovy
@@ -0,0 +1,48 @@
+package com.camnter.gradle.plugin.r2
+
+import com.android.build.gradle.*
+import org.gradle.api.Project
+
+/**
+ * @author CaMnter
+ */
+
+class PluginUtils {
+
+    static int dispatchPlugin(Project project, Closure featureClosure, Closure appClosure,
+            Closure libraryClosure) {
+        project.plugins.all {
+            if (it instanceof FeaturePlugin) {
+                FeatureExtension featureExtension = project.extensions.getByType(
+                        FeatureExtension.class)
+                featureClosure.call(featureExtension)
+            } else if (it instanceof AppPlugin) {
+                AppExtension appExtension = project.extensions.getByType(AppExtension.class)
+                appClosure.call(appExtension)
+            } else if (it instanceof LibraryPlugin) {
+                LibraryExtension libraryExtension = project.extensions.getByType(
+                        LibraryExtension.class)
+                libraryClosure.call(libraryExtension)
+            }
+        }
+    }
+
+    static void dispatchSystem(Closure osXClosure,
+            Closure linuxClosure,
+            Closure windowClosure) {
+        String system = System.getProperty("os.name")
+        switch (system) {
+            case "Mac OS X":
+                osXClosure.call()
+                break
+            case "Linux":
+                linuxClosure.call()
+                break
+            case "Windows":
+                windowClosure.call()
+                break
+            default:
+                return
+        }
+    }
+}
