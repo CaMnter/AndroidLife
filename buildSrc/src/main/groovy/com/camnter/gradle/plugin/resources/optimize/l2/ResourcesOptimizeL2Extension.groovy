95a40af0b8eb9bf2f118251424acc08f8b3f1b22
CaMnter <yuanyu.camnter@gmail.com>
2018-01-20 21:56:01 +0800

    [Add] transplant resources-optimize-l2-plugin plugin to buildSrc module

---
+++ buildSrc/src/main/groovy/com/camnter/gradle/plugin/resources/optimize/l2/ResourcesOptimizeL2Extension.groovy
@@ -0,0 +1,16 @@
+package com.camnter.gradle.plugin.resources.optimize.l2
+
+/**
+ * @author CaMnter
+ */
+
+class ResourcesOptimizeL2Extension {
+    def webpConvert = false
+    def debugResourcesOptimize = false
+    // 100 kb
+    def maxSize = 100
+
+    def cwebpPath = ''
+    def guetzliPath = ''
+    def pngquantPath = ''
+}
