0c73d38c3def245980ecbebb32341a39e2dfa77b
CaMnter <yuanyu.camnter@gmail.com>
2018-01-21 17:38:56 +0800

    [Add] transplant resources-size-plugin to buildSrc module

---
+++ buildSrc/src/main/groovy/com/camnter/gradle/plugin/resources/size/ResourcesSizeExtension.groovy
@@ -0,0 +1,11 @@
+package com.camnter.gradle.plugin.resources.size;
+
+/**
+ * @author CaMnter
+ */
+
+class ResourcesSizeExtension {
+    def debugAble = true
+    // 100 kb
+    def maxSize = 100
+}
