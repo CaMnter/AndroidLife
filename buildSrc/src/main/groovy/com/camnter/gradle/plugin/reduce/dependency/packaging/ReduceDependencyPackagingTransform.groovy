5c5aa415eff70deba844c854731830db14f85ef9
CaMnter <yuanyu.camnter@gmail.com>
2018-01-29 21:57:41 +0800

    [Fix] problem with the applicationId in buildSrc module

--- buildSrc/src/main/groovy/com/camnter/gradle/plugin/reduce/dependency/packaging/ReduceDependencyPackagingTransform.groovy
+++ buildSrc/src/main/groovy/com/camnter/gradle/plugin/reduce/dependency/packaging/ReduceDependencyPackagingTransform.groovy
@@ -3,35 +3,19 @@ package com.camnter.gradle.plugin.reduce.dependency.packaging
 import com.android.build.api.transform.*
 import com.android.build.gradle.api.BaseVariant
 import com.android.build.gradle.internal.pipeline.TransformManager
-import com.android.utils.FileUtils
 import groovy.io.FileType
+import org.apache.commons.io.FileUtils
 import org.gradle.api.DomainObjectSet
 import org.gradle.api.Project
 
 class ReduceDependencyPackagingTransform extends Transform {
 
-    def applicationId
+    Project project
+    DomainObjectSet<BaseVariant> variants
 
     ReduceDependencyPackagingTransform(Project project, DomainObjectSet<BaseVariant> variants) {
-        applicationId = variants.first().applicationId
-
-        printf "%-51s = %s\n",
-                ['[ReduceDependencyPackagingPlugin]   [applicationId]', applicationId]
-        def manifestFile = project.file("src/main/AndroidManifest.xml")
-        printf "%-51s = %s\n",
-                ['[ReduceDependencyPackagingPlugin]   [manifestFile]', "${manifestFile.path}   [exists] = ${manifestFile.exists()}"]
-        if (manifestFile.exists()) {
-            def parsedManifest = new XmlParser().parse(
-                    new InputStreamReader(new FileInputStream(manifestFile), "utf-8"))
-            if (parsedManifest != null) {
-                def packageName = parsedManifest.attribute("package")
-                if (packageName != null) {
-                    applicationId = packageName
-                    printf "%-51s = %s\n",
-                            ['[ReduceDependencyPackagingPlugin]   [applicationId]', applicationId]
-                }
-            }
-        }
+        this.project = project
+        this.variants = variants
     }
 
     @Override
@@ -62,6 +46,29 @@ class ReduceDependencyPackagingTransform extends Transform {
             transformInvocation.outputProvider.deleteAll()
         }
 
+        String applicationId = variants.first().applicationId
+
+        printf "%-52s = %s\n",
+                ['[ReduceDependencyPackagingPlugin]   [applicationId]', applicationId]
+        def manifestFile = project.file("src/main/AndroidManifest.xml")
+        printf "%-52s = %s\n",
+                ['[ReduceDependencyPackagingPlugin]   [manifestFile]', "${manifestFile.path}   [exists] = ${manifestFile.exists()}"]
+        if (manifestFile.exists()) {
+            def parsedManifest = new XmlParser().parse(
+                    new InputStreamReader(new FileInputStream(manifestFile), "utf-8"))
+            if (parsedManifest != null) {
+                def packageName = parsedManifest.attribute("package")
+                if (packageName != null) {
+                    applicationId = packageName
+                    printf "%-52s = %s\n",
+                            ['[ReduceDependencyPackagingPlugin]   [applicationId]', applicationId]
+                }
+            }
+        }
+        applicationId = applicationId.replaceAll("\\.", String.valueOf(File.separatorChar))
+        printf "%-52s = %s\n\n",
+                ['[ReduceDependencyPackagingPlugin]   [applicationId]', applicationId]
+
         transformInvocation.inputs.each {
             it.directoryInputs.each { DirectoryInput directoryInput ->
                 directoryInput.file.traverse(type: FileType.FILES) {
@@ -71,18 +78,25 @@ class ReduceDependencyPackagingTransform extends Transform {
                             directoryInput.contentTypes,
                             directoryInput.scopes,
                             Format.DIRECTORY)
-                    printf "%-51s = %s\n",
-                            ['[ReduceDependencyPackagingPlugin]   [entryName]', entryName]
-                    printf "%-51s = %s\n",
-                            ['[ReduceDependencyPackagingPlugin]   [destName]', destName]
-                    printf "%-51s = %s\n",
-                            ['[ReduceDependencyPackagingPlugin]   [output]', output.path]
-                    printf "%-51s = %s\n",
-                            ['[ReduceDependencyPackagingPlugin]   [input]', it.path]
                     // check bundle
+                    def copySuccess = false
                     if (entryName.contains(applicationId)) {
                         FileUtils.copyFile(it, output)
+                        copySuccess = true
                     }
+
+                    printf "%-52s = %s\n",
+                            ['[ReduceDependencyPackagingPlugin]   [copySuccess]', copySuccess]
+                    printf "%-52s = %s\n",
+                            ['[ReduceDependencyPackagingPlugin]   [directoryInput]', directoryInput.file.path]
+                    printf "%-52s = %s\n",
+                            ['[ReduceDependencyPackagingPlugin]   [entryName]', entryName]
+                    printf "%-52s = %s\n",
+                            ['[ReduceDependencyPackagingPlugin]   [destName]', destName]
+                    printf "%-52s = %s\n",
+                            ['[ReduceDependencyPackagingPlugin]   [output]', output.path]
+                    printf "%-52s = %s\n\n",
+                            ['[ReduceDependencyPackagingPlugin]   [input]', it.path]
                 }
             }
         }
