83e5af7a20aba41cf31085227bb0651f6d2d5085
CaMnter <yuanyu.camnter@gmail.com>
2018-03-05 19:22:00 +0800

    [Add] hook-ams-for-activity-plugin-plugin module

---
+++ hook-ams-for-activity-plugin-plugin/src/main/java/com/camnter/hook/ams/f/activity/plugin/plugin/FirstPluginActivity.java
@@ -0,0 +1,31 @@
+package com.camnter.hook.ams.f.activity.plugin.plugin;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * @author CaMnter
+ */
+
+public class FirstPluginActivity extends Activity {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final LinearLayout rootLayout = new LinearLayout(this);
+        rootLayout.setBackgroundColor(0xffffffff);
+        final TextView text = new TextView(this);
+        text.setTextColor(0xff000000);
+        text.setText("First plugin activity");
+        text.setPadding(15, 15, 15, 15);
+        rootLayout.addView(text);
+        this.setContentView(rootLayout);
+    }
+
+}
