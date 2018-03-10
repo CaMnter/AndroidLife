e1d0e41036664486b82e3df7c58645e846983a8e
CaMnter <yuanyu.camnter@gmail.com>
2018-03-10 19:20:48 +0800

    [Add] hook-loadedapk-classloader-plugin module

---
+++ hook-loadedapk-classloader-plugin/src/main/java/com/camnter/hook/loadedapk/classloader/plugin/SecondPluginActivity.java
@@ -0,0 +1,30 @@
+package com.camnter.hook.loadedapk.classloader.plugin;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * @author CaMnter
+ */
+
+public class SecondPluginActivity extends Activity {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final LinearLayout rootLayout = new LinearLayout(this);
+        rootLayout.setBackgroundColor(0xffffffff);
+        final TextView text = new TextView(this);
+        text.setTextColor(0xff000000);
+        text.setText("Second plugin activity");
+        text.setPadding(15, 15, 15, 15);
+        rootLayout.addView(text);
+        this.setContentView(rootLayout);
+    }
+
+}
