7a30311d2e2b546500527a23c10ebe01638a62eb
CaMnter <yuanyu.camnter@gmail.com>
2018-03-13 10:57:38 +0800

    [Remove] useless code in hook-ams-for-service-plugin-host module

--- plugin-life/hook-ams-for-service-plugin/hook-ams-for-service-plugin-host/src/main/java/com/camnter/hook/ams/f/service/plugin/host/SmartApplication.java
+++ plugin-life/hook-ams-for-service-plugin/hook-ams-for-service-plugin-host/src/main/java/com/camnter/hook/ams/f/service/plugin/host/SmartApplication.java
@@ -2,16 +2,9 @@ package com.camnter.hook.ams.f.service.plugin.host;
 
 import android.app.Application;
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.text.TextUtils;
 import com.camnter.hook.ams.f.service.plugin.host.hook.AMSHooker;
 import com.camnter.hook.ams.f.service.plugin.host.hook.BaseDexClassLoaderHooker;
-import dalvik.system.BaseDexClassLoader;
-import dalvik.system.DexClassLoader;
-import dalvik.system.PathClassLoader;
 import java.io.File;
-import java.lang.reflect.Array;
-import java.lang.reflect.Field;
 
 /**
  * @author CaMnter
@@ -20,8 +13,6 @@ import java.lang.reflect.Field;
 public class SmartApplication extends Application {
 
     private static Context BASE;
-    private DexClassLoader dexClassLoader;
-
 
     /**
      * Set the base context for this ContextWrapper.  All calls will then be
@@ -50,120 +41,6 @@ public class SmartApplication extends Application {
         }
 
     }
-
-
-    private void loadDex() {
-        try {
-            String dir = null;
-            final File cacheDir = this.getExternalCacheDir();
-            final File filesDir = this.getExternalFilesDir("");
-            if (cacheDir != null) {
-                dir = cacheDir.getAbsolutePath();
-            } else {
-                if (filesDir != null) {
-                    dir = filesDir.getAbsolutePath();
-                }
-            }
-            if (TextUtils.isEmpty(dir)) return;
-
-            // assets 的 hook-ams-for-service-plugin-plugin.apk 拷贝到 /storage/sdcard0/Android/data/[package name]/cache
-            // 或者  /storage/sdcard0/Android/data/[package name]/files
-            final File dexPath = new File(
-                dir + File.separator + "hook-ams-for-service-plugin-plugin.apk");
-            final String dexAbsolutePath = dexPath.getAbsolutePath();
-            AssetsUtils.copyAssets(this, "hook-ams-for-service-plugin-plugin.apk",
-                dexAbsolutePath);
-
-            // /data/data/[package name]/app_hook-ams-for-service-plugin-plugin
-            final File optimizedDirectory = this.getDir("hook-ams-for-service-plugin-plugin",
-                MODE_PRIVATE);
-
-            this.dexClassLoader = new DexClassLoader(
-                dexPath.getAbsolutePath(),
-                optimizedDirectory.getAbsolutePath(),
-                null,
-                this.getClassLoader()
-            );
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    private synchronized void injectAboveEqualApiLevel14() {
-        final PathClassLoader pathClassLoader
-            = (PathClassLoader) SmartApplication.class.getClassLoader();
-        try {
-            // 插桩
-            final Object dexElements = combineArray(
-                getDexElements(getPathList(pathClassLoader)),
-                getDexElements(getPathList(this.dexClassLoader))
-            );
-            final Object pathList = getPathList(pathClassLoader);
-            setField(pathList, pathList.getClass(), "dexElements", dexElements);
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    private static Object getPathList(@NonNull  final Object baseDexClassLoader)
-        throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException,
-               ClassNotFoundException {
-        return getField(baseDexClassLoader, BaseDexClassLoader.class,
-            "pathList");
-    }
-
-
-    private static Object getDexElements(@NonNull  final Object paramObject)
-        throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException {
-        return getField(paramObject, paramObject.getClass(), "dexElements");
-    }
-
-
-    private static Object getField(@NonNull final Object object,
-                                   @NonNull final Class<?> classloader,
-                                   @NonNull final String field)
-        throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
-        final Field localField = classloader.getDeclaredField(field);
-        localField.setAccessible(true);
-        return localField.get(object);
-    }
-
-
-    @SuppressWarnings("SameParameterValue")
-    private static void setField(@NonNull final Object object,
-                                 @NonNull  final Class<?> classloader,
-                                 @NonNull final String field,
-                                 final Object value)
-        throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
-        final Field localField = classloader.getDeclaredField(field);
-        localField.setAccessible(true);
-        localField.set(object, value);
-    }
-
-
-    private static Object combineArray(@NonNull final Object arrayLhs,
-                                       @NonNull final Object arrayRhs) {
-        final Class<?> localClass = arrayLhs.getClass().getComponentType();
-        final int i = Array.getLength(arrayLhs);
-        final int j = i + Array.getLength(arrayRhs);
-        final Object result = Array.newInstance(localClass, j);
-        for (int k = 0; k < j; ++k) {
-            if (k < i) {
-                Array.set(result, k, Array.get(arrayLhs, k));
-            } else {
-                Array.set(result, k, Array.get(arrayRhs, k - i));
-            }
-        }
-        return result;
-    }
-
-
-    public DexClassLoader getDexClassLoader() {
-        return this.dexClassLoader;
-    }
-
 
     public static Context getContext() {
         return BASE;
