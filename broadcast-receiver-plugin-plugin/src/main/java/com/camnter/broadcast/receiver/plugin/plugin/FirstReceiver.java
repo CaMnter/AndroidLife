fa37a4b42433f8472bef0df4c173fe3ee1faff99
CaMnter <yuanyu.camnter@gmail.com>
2018-03-14 11:20:36 +0800

    [Add] broadcast-receiver-plugin-plugin module

---
+++ broadcast-receiver-plugin-plugin/src/main/java/com/camnter/broadcast/receiver/plugin/plugin/FirstReceiver.java
@@ -0,0 +1,25 @@
+package com.camnter.broadcast.receiver.plugin.plugin;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * @author CaMnter
+ */
+
+public class FirstReceiver extends BroadcastReceiver {
+
+    private static final String TAG = FirstReceiver.class.getSimpleName();
+
+    public static final String INTENT_EXTRA_MESSAGE = "message";
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final String message = intent.getStringExtra(INTENT_EXTRA_MESSAGE);
+        Toast.makeText(context, TAG + ":" + message, Toast.LENGTH_SHORT).show();
+    }
+
+}
