d31b138e2e71323bdf5060d790a76773c9181c5a
CaMnter <yuanyu.camnter@gmail.com>
2018-03-24 15:50:18 +0800

    [Add] comments of RemoteContentProvider in virtualapk-core-library module

--- virtualapk-core-library/src/main/java/com/didi/virtualapk/delegate/RemoteContentProvider.java
+++ virtualapk-core-library/src/main/java/com/didi/virtualapk/delegate/RemoteContentProvider.java
@@ -37,6 +37,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 插桩 ContentProvider
+ *
  * Created by renyugang on 16/12/7.
  */
 
@@ -60,6 +62,24 @@ public class RemoteContentProvider extends ContentProvider {
     }
 
 
+    /**
+     * 获取插件 ContentProvider
+     *
+     * 1. 解析复合 Uri 协议，获取其中包含的插件 Uri 协议
+     * 2. 获取插件 插件 Uri 协议 中的 Authority
+     * 3. 根据 插件 Uri Authority，拿出插件 ContentProvider 缓存
+     *
+     * 4. 当前面没获取到插件 ContentProvider 缓存的时候，根据 插件 Uri 协议包含的该 ContentProvider 所在
+     * -  apk file 的绝对路径，获取到对应的 LoadedPlugin。如果还没有 LoadedPlugin，需要根据 apk file path
+     * -  加载出 LoadedPlugin
+     *
+     * 5. 在保证有 LoadedPlugin 缓存的情况下。去获取该 插件 ContentProvider 对应的 ProviderInfo 信息
+     * 6. 给 主线程 发消息，主线程获取对应的 LoadedPlugin 缓存，然后加载出对应的 插件 ContentProvider class
+     * -  手动调用 ContentProvider # attachInfo(...)。最后加入到 插件 ContentProvider 集合内
+     *
+     * @param uri uri
+     * @return ContentProvider
+     */
     private ContentProvider getContentProvider(final Uri uri) {
         final PluginManager pluginManager = PluginManager.getInstance(getContext());
         Uri pluginUri = Uri.parse(uri.getQueryParameter(KEY_URI));
@@ -105,6 +125,15 @@ public class RemoteContentProvider extends ContentProvider {
     }
 
 
+    /**
+     * 获取插件 ContentProvider 后
+     * 解析复合 Uri 协议，获取其中包含的插件 Uri 协议
+     *
+     * 手动调用 插件 ContentProvider # getType(...)
+     *
+     * @param uri uri
+     * @return String
+     */
     @Override
     public String getType(Uri uri) {
         ContentProvider provider = getContentProvider(uri);
@@ -117,6 +146,19 @@ public class RemoteContentProvider extends ContentProvider {
     }
 
 
+    /**
+     * 获取插件 ContentProvider 后
+     * 解析复合 Uri 协议，获取其中包含的插件 Uri 协议
+     *
+     * 手动调用 插件 ContentProvider # query(...)
+     *
+     * @param uri uri
+     * @param projection projection
+     * @param selection selection
+     * @param selectionArgs selectionArgs
+     * @param sortOrder sortOrder
+     * @return Cursor
+     */
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
         ContentProvider provider = getContentProvider(uri);
@@ -129,6 +171,16 @@ public class RemoteContentProvider extends ContentProvider {
     }
 
 
+    /**
+     * 获取插件 ContentProvider 后
+     * 解析复合 Uri 协议，获取其中包含的插件 Uri 协议
+     *
+     * 手动调用 插件 ContentProvider # insert(...)
+     *
+     * @param uri uri
+     * @param values values
+     * @return Uri
+     */
     @Override
     public Uri insert(Uri uri, ContentValues values) {
         ContentProvider provider = getContentProvider(uri);
@@ -141,6 +193,17 @@ public class RemoteContentProvider extends ContentProvider {
     }
 
 
+    /**
+     * 获取插件 ContentProvider 后
+     * 解析复合 Uri 协议，获取其中包含的插件 Uri 协议
+     *
+     * 手动调用 插件 ContentProvider # delete(...)
+     *
+     * @param uri uri
+     * @param selection selection
+     * @param selectionArgs selectionArgs
+     * @return int
+     */
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
         ContentProvider provider = getContentProvider(uri);
@@ -153,6 +216,18 @@ public class RemoteContentProvider extends ContentProvider {
     }
 
 
+    /**
+     * 获取插件 ContentProvider 后
+     * 解析复合 Uri 协议，获取其中包含的插件 Uri 协议
+     *
+     * 手动调用 插件 ContentProvider # update(...)
+     *
+     * @param uri uri
+     * @param values values
+     * @param selection selection
+     * @param selectionArgs selectionArgs
+     * @return int
+     */
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
         ContentProvider provider = getContentProvider(uri);
@@ -165,6 +240,16 @@ public class RemoteContentProvider extends ContentProvider {
     }
 
 
+    /**
+     * 获取插件 ContentProvider 后
+     * 解析复合 Uri 协议，获取其中包含的插件 Uri 协议
+     *
+     * 手动调用 插件 ContentProvider # bulkInsert(...)
+     *
+     * @param uri uri
+     * @param values values
+     * @return int
+     */
     @Override
     public int bulkInsert(Uri uri, ContentValues[] values) {
         ContentProvider provider = getContentProvider(uri);
@@ -177,6 +262,17 @@ public class RemoteContentProvider extends ContentProvider {
     }
 
 
+    /**
+     * 反射替换 ContentProviderOperation 集合中所有的 ContentProviderOperation # Uri mUri
+     *
+     * 在 ContentProviderOperation 集合存在数据的情况下
+     * 取出每个 ContentProviderOperation 对应的插件 ContentProvider
+     * 手动调用 ContentProvider # applyBatch(...)
+     *
+     * @param operations operations
+     * @return ContentProviderResult[]
+     * @throws OperationApplicationException OperationApplicationException
+     */
     @NonNull
     @Override
     public ContentProviderResult[] applyBatch(ArrayList<ContentProviderOperation> operations)
@@ -203,6 +299,17 @@ public class RemoteContentProvider extends ContentProvider {
     }
 
 
+    /**
+     * 获取插件 ContentProvider 后
+     * 解析复合 Uri 协议，获取其中包含的插件 Uri 协议
+     *
+     * 手动调用 插件 ContentProvider # call(...)
+     *
+     * @param method method
+     * @param arg arg
+     * @param extras extras
+     * @return Bundle
+     */
     @Override
     public Bundle call(String method, String arg, Bundle extras) {
         Log.d(TAG, "call " + method + " with extras : " + extras);
