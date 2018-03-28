e366423adfee401a499ce4b8ad684ac5c18adb84
CaMnter <yuanyu.camnter@gmail.com>
2018-03-28 17:04:15 +0800

    [Add] comments of PluginManager in virtualapk-core-library module

--- virtualapk-core-library/src/main/java/com/didi/virtualapk/PluginManager.java
+++ virtualapk-core-library/src/main/java/com/didi/virtualapk/PluginManager.java
@@ -53,6 +53,7 @@ import java.util.concurrent.ConcurrentHashMap;
 /**
  * Created by renyugang on 16/8/9.
  */
+@SuppressWarnings("DanglingJavadoc")
 public class PluginManager {
 
     public static final String TAG = "PluginManager";
@@ -62,8 +63,22 @@ public class PluginManager {
     // Context of host app
     private Context mContext;
     private ComponentsHandler mComponentsHandler;
+
+    /**
+     * 所有 加载过的 Apk 信息
+     */
     private Map<String, LoadedPlugin> mPlugins = new ConcurrentHashMap<>();
 
+    /**
+     * 缓存被 hook 掉的
+     * Instrumentation
+     * IActivityManager binder
+     * IContentProvider binder
+     *
+     * 由于 都是用动态代理 hook
+     * 如果想不走动态代理的逻辑
+     * 就需要保存 未代理的 原引用
+     */
     private Instrumentation mInstrumentation; // Hooked instrumentation
     private IActivityManager mActivityManager; // Hooked IActivityManager binder
     private IContentProvider mIContentProvider; // Hooked IContentProvider binder
@@ -93,6 +108,13 @@ public class PluginManager {
     }
 
 
+    /**
+     * 准备
+     *
+     * 1. 保存宿主 context
+     * 2. Hook Instrumentation 和 ActivityThread # H # Handler.Callback mCallback
+     * 3. 根据 是否是 8.0.0  版本，选择 Hook AMS 的方案。但是，都会 Hook AMS
+     */
     private void prepare() {
         Systems.sHostContext = getHostContext();
         this.hookInstrumentationAndHandler();
@@ -121,8 +143,73 @@ public class PluginManager {
 
     /**
      * hookSystemServices, but need to compatible with Android O in future.
+     *
+     * Android 7.1.1 一下 hook AMS
+     *
+     * AMS 的 远程 Binder Proxy 类 放在了 ActivityManagerNative 内
      */
     private void hookSystemServices() {
+        /**
+         * ActivityManagerNative 部分源码
+         *
+         * -----------------------------------------------------------------------------------------
+         *
+         * public abstract class ActivityManagerNative extends Binder implements IActivityManager{
+         *
+         *      ...
+         *
+         *      private static final Singleton<IActivityManager> gDefault = new Singleton<IActivityManager>() {
+         *          protected IActivityManager create() {
+         *              IBinder b = ServiceManager.getService("activity");
+         *              if (false) {
+         *                  Log.v("ActivityManager", "default service binder = " + b);
+         *              }
+         *              IActivityManager am = asInterface(b);
+         *              if (false) {
+         *                  Log.v("ActivityManager", "default service = " + am);
+         *              }
+         *              return am;
+         *          }
+         *      };
+         *
+         *      ...
+         *
+         *
+         * }
+         *
+         * -----------------------------------------------------------------------------------------
+         *
+         * Singleton 的类结构
+         *
+         * -----------------------------------------------------------------------------------------
+         *
+         * package android.util;
+         *
+         *  **
+         *  * Singleton helper class for lazily initialization.
+         *  *
+         *  * Modeled after frameworks/base/include/utils/Singleton.h
+         *  *
+         *  * @hide
+         *  **
+         *
+         * public abstract class Singleton<T> {
+         *      private T mInstance;
+         *
+         *      protected abstract T create();
+         *
+         *      public final T get() {
+         *          synchronized (this) {
+         *              if (mInstance == null) {
+         *                  mInstance = create();
+         *              }
+         *                  return mInstance;
+         *          }
+         *      }
+         * }
+         *
+         * -----------------------------------------------------------------------------------------
+         */
         try {
             Singleton<IActivityManager> defaultSingleton = (Singleton<IActivityManager>) ReflectUtil
                 .getField(ActivityManagerNative.class, null, "gDefault");
@@ -142,7 +229,68 @@ public class PluginManager {
     }
 
 
+    /**
+     * Android 8.0.0 以上 hook AMS
+     *
+     * AMS 的 远程 Binder Proxy 类 放在了 ActivityManager 内
+     */
     private void hookAMSForO() {
+        /**
+         * ActivityManager 部分源码
+         *
+         * -----------------------------------------------------------------------------------------
+         *
+         * public class ActivityManager {
+         *
+         *      ...
+         *
+         *      private static final Singleton<IActivityManager> IActivityManagerSingleton =
+         *              new Singleton<IActivityManager>() {
+         *                  @Override
+         *                      protected IActivityManager create() {
+         *                          final IBinder b = ServiceManager.getService(Context.ACTIVITY_SERVICE);
+         *                          final IActivityManager am = IActivityManager.Stub.asInterface(b);
+         *                          return am;
+         *                      }
+         *              };
+         *
+         *      ...
+         *
+         * }
+         *
+         * -----------------------------------------------------------------------------------------
+         *
+         * Singleton 的类结构
+         *
+         * -----------------------------------------------------------------------------------------
+         *
+         * package android.util;
+         *
+         *  **
+         *  * Singleton helper class for lazily initialization.
+         *  *
+         *  * Modeled after frameworks/base/include/utils/Singleton.h
+         *  *
+         *  * @hide
+         *  **
+         *
+         * public abstract class Singleton<T> {
+         *      private T mInstance;
+         *
+         *      protected abstract T create();
+         *
+         *      public final T get() {
+         *          synchronized (this) {
+         *              if (mInstance == null) {
+         *                  mInstance = create();
+         *              }
+         *                  return mInstance;
+         *          }
+         *      }
+         * }
+         *
+         * -----------------------------------------------------------------------------------------
+         */
         try {
             Singleton<IActivityManager> defaultSingleton = (Singleton<IActivityManager>) ReflectUtil
                 .getField(ActivityManager.class, null, "IActivityManagerSingleton");
@@ -156,6 +304,16 @@ public class PluginManager {
     }
 
 
+    /**
+     * Hook Instrumentation 和 ActivityThread # H # Handler.Callback mCallback
+     *
+     * 用 VAInstrumentation 去 hook
+     * VAInstrumentation 继承了 Instrumentation 类，实现了 Handler.Callback 接口
+     *
+     * 唯一处理的地方就是
+     * 判断 class full name 是否含有 lbe
+     * 不能运行在 lbe 上
+     */
     private void hookInstrumentationAndHandler() {
         try {
             Instrumentation baseInstrumentation = ReflectUtil.getInstrumentation(this.mContext);
@@ -176,6 +334,14 @@ public class PluginManager {
     }
 
 
+    /**
+     * 用于动态代理 RemoteContentProvider 的远程 Binder proxy
+     *
+     * hook 点是
+     *
+     * ActivityThread # ArrayMap<ProviderKey, ProviderClientRecord> mProviderMap
+     * ActivityThread # ProviderClientRecord # IContentProvider mProvider
+     */
     private void hookIContentProviderAsNeeded() {
         Uri uri = Uri.parse(PluginContentResolver.getUri(mContext));
         mContext.getContentResolver().call(uri, "wakeup", null, null);
@@ -224,7 +390,11 @@ public class PluginManager {
      * load a plugin into memory, then invoke it's Application.
      *
      * @param apk the file of plugin, should end with .apk
-     * @throws Exception
+     * @throws Exception Exception
+     *
+     * 加载 Apk 文件
+     *
+     * 将 Apk 文件加载成 VA 识别的 LoadedPlugin
      */
     public void loadPlugin(File apk) throws Exception {
         if (null == apk) {
@@ -247,22 +417,49 @@ public class PluginManager {
     }
 
 
+    /**
+     * 根据 Intent 抽取出对应的 LoadedPlugin
+     * 可以理解为抽取出对应的 Apk 信息
+     *
+     * @param intent intent
+     * @return LoadedPlugin
+     */
     public LoadedPlugin getLoadedPlugin(Intent intent) {
         ComponentName component = PluginUtil.getComponent(intent);
         return getLoadedPlugin(component.getPackageName());
     }
 
 
+    /**
+     * 根据 ComponentName 抽取出对应的 LoadedPlugin
+     * 可以理解为抽取出对应的 Apk 信息
+     *
+     * @param component component
+     * @return LoadedPlugin
+     */
     public LoadedPlugin getLoadedPlugin(ComponentName component) {
         return this.getLoadedPlugin(component.getPackageName());
     }
 
 
+    /**
+     * 根据 packageName 抽取出对应的 LoadedPlugin
+     * 可以理解为抽取出对应的 Apk 信息
+     *
+     * @param packageName packageName
+     * @return LoadedPlugin
+     */
     public LoadedPlugin getLoadedPlugin(String packageName) {
         return this.mPlugins.get(packageName);
     }
 
 
+    /**
+     * 获取所有内存加载过的 Apk 信息
+     * 即，LoadedPlugin
+     *
+     * @return List<LoadedPlugin>
+     */
     public List<LoadedPlugin> getAllLoadedPlugins() {
         List<LoadedPlugin> list = new ArrayList<>();
         list.addAll(mPlugins.values());
@@ -270,21 +467,50 @@ public class PluginManager {
     }
 
 
+    /**
+     * 获取 宿主 Context
+     *
+     * @return Context
+     */
     public Context getHostContext() {
         return this.mContext;
     }
 
 
+    /**
+     * 返回被 hook 掉的 Instrumentation
+     * 就是原来的 Instrumentation
+     *
+     * 以便可以执行 非动态代理 原逻辑
+     *
+     * @return Instrumentation
+     */
     public Instrumentation getInstrumentation() {
         return this.mInstrumentation;
     }
 
 
+    /**
+     * 返回被 hook 掉的 IActivityManager
+     * 就是原来的 IActivityManager
+     *
+     * 以便可以执行 非动态代理 原逻辑
+     *
+     * @return IActivityManager
+     */
     public IActivityManager getActivityManager() {
         return this.mActivityManager;
     }
 
 
+    /**
+     * 返回被 hook 掉的 RemoteContentProvider
+     * 就是原来的 RemoteContentProvider
+     *
+     * 以便可以执行 非动态代理 原逻辑
+     *
+     * @return IContentProvider
+     */
     public synchronized IContentProvider getIContentProvider() {
         if (mIContentProvider == null) {
             hookIContentProviderAsNeeded();
@@ -304,6 +530,14 @@ public class PluginManager {
     }
 
 
+    /**
+     * 从 所有内存加载过的 Apk 信息 中
+     * 寻找是否有该 intent 对应的 Activity 信息
+     *
+     * @param intent intent
+     * @param flags flags
+     * @return ResolveInfo
+     */
     public ResolveInfo resolveActivity(Intent intent, int flags) {
         for (LoadedPlugin plugin : this.mPlugins.values()) {
             ResolveInfo resolveInfo = plugin.resolveActivity(intent, flags);
@@ -316,6 +550,14 @@ public class PluginManager {
     }
 
 
+    /**
+     * 从 所有内存加载过的 Apk 信息 中
+     * 寻找是否有该 intent 对应的 Service 信息
+     *
+     * @param intent intent
+     * @param flags flags
+     * @return ResolveInfo
+     */
     public ResolveInfo resolveService(Intent intent, int flags) {
         for (LoadedPlugin plugin : this.mPlugins.values()) {
             ResolveInfo resolveInfo = plugin.resolveService(intent, flags);
@@ -328,6 +570,14 @@ public class PluginManager {
     }
 
 
+    /**
+     * 从 所有内存加载过的 Apk 信息 中
+     * 寻找是否有该 协议 对应的 ContentProvider 信息
+     *
+     * @param name name
+     * @param flags flags
+     * @return ProviderInfo
+     */
     public ProviderInfo resolveContentProvider(String name, int flags) {
         for (LoadedPlugin plugin : this.mPlugins.values()) {
             ProviderInfo providerInfo = plugin.resolveContentProvider(name, flags);
@@ -342,6 +592,13 @@ public class PluginManager {
 
     /**
      * used in PluginPackageManager, do not invoke it from outside.
+     *
+     * 从 所有内存加载过的 Apk 信息 中
+     * 寻找是否有该 intent 对应的 Activity 信息
+     *
+     * @param intent intent
+     * @param flags flags
+     * @return List<ResolveInfo>
      */
     @Deprecated
     public List<ResolveInfo> queryIntentActivities(Intent intent, int flags) {
@@ -360,6 +617,13 @@ public class PluginManager {
 
     /**
      * used in PluginPackageManager, do not invoke it from outside.
+     *
+     * 从 所有内存加载过的 Apk 信息 中
+     * 寻找是否有该 intent 对应的 Service 信息
+     *
+     * @param intent intent
+     * @param flags flags
+     * @return List<ResolveInfo>
      */
     @Deprecated
     public List<ResolveInfo> queryIntentServices(Intent intent, int flags) {
@@ -378,6 +642,13 @@ public class PluginManager {
 
     /**
      * used in PluginPackageManager, do not invoke it from outside.
+     *
+     * 从 所有内存加载过的 Apk 信息 中
+     * 寻找是否有该 intent 对应的 BroadcastReceiver 信息
+     *
+     * @param intent intent
+     * @param flags flags
+     * @return List<ResolveInfo>
      */
     @Deprecated
     public List<ResolveInfo> queryBroadcastReceivers(Intent intent, int flags) {
