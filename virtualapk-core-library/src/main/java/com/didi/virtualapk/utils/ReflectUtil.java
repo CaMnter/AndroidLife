59c78ff58b405d0fca6654153ed05befb2d69d0b
CaMnter <yuanyu.camnter@gmail.com>
2018-04-05 17:58:15 +0800

    [Add] comments of ReflectUtil in virtualapk-core-library module

--- virtualapk-core-library/src/main/java/com/didi/virtualapk/utils/ReflectUtil.java
+++ virtualapk-core-library/src/main/java/com/didi/virtualapk/utils/ReflectUtil.java
@@ -20,7 +20,6 @@ import android.app.Instrumentation;
 import android.content.Context;
 import android.os.Handler;
 import android.support.annotation.UiThread;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -32,12 +31,33 @@ public class ReflectUtil {
     public static Object sLoadedApk;
     public static Instrumentation sInstrumentation;
 
+
+    /**
+     * 反射获取 field 的值
+     * 会抛异常
+     *
+     * @param clazz clazz
+     * @param target target
+     * @param name name
+     * @return Object
+     * @throws Exception Exception
+     */
     public static Object getField(Class clazz, Object target, String name) throws Exception {
         Field field = clazz.getDeclaredField(name);
         field.setAccessible(true);
         return field.get(target);
     }
 
+
+    /**
+     * 反射获取 field 的值
+     * 不会抛异常
+     *
+     * @param clazz clazz
+     * @param target target
+     * @param name name
+     * @return Object
+     */
     public static Object getFieldNoException(Class clazz, Object target, String name) {
         try {
             return ReflectUtil.getField(clazz, target, name);
@@ -48,12 +68,35 @@ public class ReflectUtil {
         return null;
     }
 
-    public static void setField(Class clazz, Object target, String name, Object value) throws Exception {
+
+    /**
+     * 反射设置 field 的值
+     * 会抛异常
+     *
+     * @param clazz clazz
+     * @param target target
+     * @param name name
+     * @param value value
+     * @throws Exception Exception
+     */
+    public static void setField(Class clazz, Object target, String name, Object value)
+        throws Exception {
         Field field = clazz.getDeclaredField(name);
         field.setAccessible(true);
         field.set(target, value);
     }
 
+
+    /**
+     * 反射设置 field 的值
+     * 不会抛异常
+     *
+     * @param clazz clazz
+     * @param target target
+     * @param name name
+     * @param value value
+     * @throws Exception Exception
+     */
     public static void setFieldNoException(Class clazz, Object target, String name, Object value) {
         try {
             ReflectUtil.setField(clazz, target, name, value);
@@ -62,9 +105,21 @@ public class ReflectUtil {
         }
     }
 
+
+    /**
+     * 反射调用 method
+     * 会抛异常
+     *
+     * @param clazz clazz
+     * @param target target
+     * @param name name
+     * @param args args
+     * @return Object
+     * @throws Exception Exception
+     */
     @SuppressWarnings("unchecked")
     public static Object invoke(Class clazz, Object target, String name, Object... args)
-            throws Exception {
+        throws Exception {
         Class[] parameterTypes = null;
         if (args != null) {
             parameterTypes = new Class[args.length];
@@ -78,14 +133,39 @@ public class ReflectUtil {
         return method.invoke(target, args);
     }
 
+
+    /**
+     * 反射调用 method
+     * 会抛异常
+     *
+     * @param clazz clazz
+     * @param target target
+     * @param name name
+     * @param parameterTypes parameterTypes
+     * @param args args
+     * @return Object
+     * @throws Exception Exception
+     */
     @SuppressWarnings("unchecked")
     public static Object invoke(Class clazz, Object target, String name, Class[] parameterTypes, Object... args)
-            throws Exception {
+        throws Exception {
         Method method = clazz.getDeclaredMethod(name, parameterTypes);
         method.setAccessible(true);
         return method.invoke(target, args);
     }
 
+
+    /**
+     * 反射调用 method
+     * 不会抛异常
+     *
+     * @param clazz clazz
+     * @param target target
+     * @param name name
+     * @param parameterTypes parameterTypes
+     * @param args args
+     * @return Object
+     */
     @SuppressWarnings("unchecked")
     public static Object invokeNoException(Class clazz, Object target, String name, Class[] parameterTypes, Object... args) {
         try {
@@ -96,14 +176,37 @@ public class ReflectUtil {
         return null;
     }
 
+
+    /**
+     * 反射调用 构造方法
+     * 会抛异常
+     *
+     * @param clazz clazz
+     * @param parameterTypes parameterTypes
+     * @param args args
+     * @return Object
+     * @throws Exception Exception
+     */
     @SuppressWarnings("unchecked")
     public static Object invokeConstructor(Class clazz, Class[] parameterTypes, Object... args)
-            throws Exception {
+        throws Exception {
         Constructor constructor = clazz.getDeclaredConstructor(parameterTypes);
         constructor.setAccessible(true);
         return constructor.newInstance(args);
     }
 
+
+    /**
+     * 反射获取 ActivityThread
+     *
+     * 两个尝试点
+     *
+     * ActivityThread # ActivityThread sCurrentActivityThread
+     * ActivityThread # ThreadLocal<ActivityThread> sThreadLocal
+     *
+     * @param base base
+     * @return Object
+     */
     @UiThread
     public static Object getActivityThread(Context base) {
         if (sActivityThread == null) {
@@ -111,12 +214,14 @@ public class ReflectUtil {
                 Class<?> activityThreadClazz = Class.forName("android.app.ActivityThread");
                 Object activityThread = null;
                 try {
-                    activityThread = ReflectUtil.getField(activityThreadClazz, null, "sCurrentActivityThread");
+                    activityThread = ReflectUtil.getField(activityThreadClazz, null,
+                        "sCurrentActivityThread");
                 } catch (Exception e) {
                     // ignored
                 }
                 if (activityThread == null) {
-                    activityThread = ((ThreadLocal<?>) ReflectUtil.getField(activityThreadClazz, null, "sThreadLocal")).get();
+                    activityThread = ((ThreadLocal<?>) ReflectUtil.getField(activityThreadClazz,
+                        null, "sThreadLocal")).get();
                 }
                 sActivityThread = activityThread;
             } catch (Exception e) {
@@ -127,11 +232,20 @@ public class ReflectUtil {
         return sActivityThread;
     }
 
+
+    /**
+     * 反射获取 Instrumentation
+     *
+     * ActivityThread # Instrumentation getInstrumentation()
+     *
+     * @param base base
+     * @return Instrumentation
+     */
     public static Instrumentation getInstrumentation(Context base) {
         if (getActivityThread(base) != null) {
             try {
                 sInstrumentation = (Instrumentation) ReflectUtil.invoke(
-                        sActivityThread.getClass(), sActivityThread, "getInstrumentation");
+                    sActivityThread.getClass(), sActivityThread, "getInstrumentation");
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -140,14 +254,33 @@ public class ReflectUtil {
         return sInstrumentation;
     }
 
+
+    /**
+     * 反射设置 Instrumentation
+     *
+     * ActivityThread # Instrumentation mInstrumentation
+     *
+     * @param activityThread activityThread
+     * @param instrumentation instrumentation
+     */
     public static void setInstrumentation(Object activityThread, Instrumentation instrumentation) {
         try {
-            ReflectUtil.setField(activityThread.getClass(), activityThread, "mInstrumentation", instrumentation);
+            ReflectUtil.setField(activityThread.getClass(), activityThread, "mInstrumentation",
+                instrumentation);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+
+    /**
+     * 反射获取 LoadedApk
+     *
+     * ContextImpl # @NonNull LoadedApk mPackageInfo
+     *
+     * @param base base
+     * @return Object
+     */
     public static Object getPackageInfo(Context base) {
         if (sLoadedApk == null) {
             try {
@@ -160,10 +293,20 @@ public class ReflectUtil {
         return sLoadedApk;
     }
 
+
+    /**
+     * 反射设置 Handler # Handler.Callback mCallback
+     *
+     * 主要为了 hook ActivityThread # H # Handler.Callback mCallback
+     *
+     * @param base base
+     * @param callback callback
+     */
     public static void setHandlerCallback(Context base, Handler.Callback callback) {
         try {
             Object activityThread = getActivityThread(base);
-            Handler mainHandler = (Handler) ReflectUtil.invoke(activityThread.getClass(), activityThread, "getHandler", (Object[])null);
+            Handler mainHandler = (Handler) ReflectUtil.invoke(activityThread.getClass(),
+                activityThread, "getHandler", (Object[]) null);
             ReflectUtil.setField(Handler.class, mainHandler, "mCallback", callback);
         } catch (Exception e) {
             e.printStackTrace();
